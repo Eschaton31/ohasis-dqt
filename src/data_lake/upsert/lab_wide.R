4856ae1ca8bc21891158828df3a98fba1e55b727
jrpalo.doh <jrpalo@doh.gov.ph>
2022-03-14 11:42:50 +0800

    added upsert for laboratory test

---
+++ src/data_lake/upsert/lab_wide.R
@@ -0,0 +1,96 @@
+##  CD4 Data -------------------------------------------------------------------
+
+continue <- 0
+id_col   <- "REC_ID"
+object   <- tbl(db_conn, dbplyr::in_schema("ohasis_interim", "px_record")) %>%
+   filter(
+      (CREATED_AT >= snapshot_old & CREATED_AT <= snapshot_new) |
+         (UPDATED_AT >= snapshot_old & UPDATED_AT <= snapshot_new) |
+         (DELETED_AT >= snapshot_old & DELETED_AT <= snapshot_new)
+   ) %>%
+   select(
+      REC_ID,
+      PATIENT_ID,
+      CREATED_AT,
+      UPDATED_AT,
+      DELETED_AT
+   )
+
+# get number of affected rows
+if ((object %>% count() %>% collect())$n > 0) {
+   continue <- 1
+   object   <- object %>%
+      mutate(
+         SNAPSHOT = case_when(
+            !is.na(DELETED_AT) ~ DELETED_AT,
+            !is.na(UPDATED_AT) ~ UPDATED_AT,
+            TRUE ~ CREATED_AT
+         ),
+      ) %>%
+      collect() %>%
+      inner_join(
+         y  = tbl(db_conn, dbplyr::in_schema("ohasis_interim", "px_labs")) %>%
+            select(
+               REC_ID,
+               TEST         = LAB_TEST,
+               DATE         = LAB_DATE,
+               RESULT       = LAB_RESULT,
+               RESULT_OTHER = LAB_RESULT_OTHER
+            ) %>%
+            mutate(
+               DATE   = as.Date(DATE),
+               RESULT = case_when(
+                  TEST == "1" & RESULT == "1" ~ "1_Reactive",
+                  TEST == "1" & RESULT == "2" ~ "2_Non-reactive",
+                  TEST == "3" & RESULT == "1" ~ "1_Reactive",
+                  TEST == "3" & RESULT == "2" ~ "2_Non-reactive",
+                  TRUE ~ RESULT %>% as.character()
+               ),
+               TEST   = case_when(
+                  TEST == "1" ~ "HBSAG",
+                  TEST == "2" ~ "CREA",
+                  TEST == "3" ~ "SYPH",
+                  TEST == "4" ~ "VIRAL",
+                  TEST == "5" ~ "CD4",
+                  TEST == "6" ~ "XRAY",
+                  TEST == "7" ~ "XPERT",
+                  TEST == "8" ~ "DSSM",
+                  TEST == "9" ~ "HIVDR",
+                  TEST == "10" ~ "HEMOG",
+                  TRUE ~ TEST %>% as.character()
+               ),
+            ) %>%
+            pivot_wider(
+               id_cols     = REC_ID,
+               names_from  = TEST,
+               values_from = c(DATE, RESULT, RESULT_OTHER)
+            ) %>%
+            select(
+               REC_ID,
+               LAB_HEMOG_DATE     = DATE_HEMOG,
+               LAB_HEMOG_RESULT   = RESULT_HEMOG,
+               LAB_VIRAL_DATE     = DATE_VIRAL,
+               LAB_VIRAL_RESULT   = RESULT_VIRAL,
+               LAB_CD4_DATE       = DATE_CD4,
+               LAB_CD4_RESULT     = RESULT_CD4,
+               LAB_CREA_DATE      = DATE_CREA,
+               LAB_CREA_RESULT    = RESULT_CREA,
+               LAB_CREA_CLEARANCE = RESULT_OTHER_CREA,
+               LAB_HBSAG_DATE     = DATE_HBSAG,
+               LAB_HBSAG_RESULT   = RESULT_HBSAG,
+               LAB_XRAY_DATE      = DATE_XRAY,
+               LAB_XRAY_RESULT    = RESULT_XRAY,
+               LAB_XPERT_DATE     = DATE_XPERT,
+               LAB_XPERT_RESULT   = RESULT_XPERT,
+               LAB_DSSM_DATE      = DATE_DSSM,
+               LAB_DSSM_RESULT    = RESULT_DSSM,
+               LAB_HIVDR_DATE     = DATE_HIVDR,
+               LAB_HIVDR_RESULT   = RESULT_HIVDR,
+               LAB_SYPH_DATE      = DATE_SYPH,
+               LAB_SYPH_RESULT    = RESULT_SYPH,
+               LAB_SYPH_TITER     = RESULT_OTHER_SYPH,
+            ) %>%
+            collect(),
+         by = "REC_ID"
+      )
+}
