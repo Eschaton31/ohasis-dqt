d4df1442bfbded4deaa2d170a405e9956ce35cbb
jrpalo.doh <jrpalo@doh.gov.ph>
2022-03-18 15:01:19 +0800

    added art start + latest queries

---
+++ src/official/harp_tx/data_dl/art_first.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM (SELECT data.CENTRAL_ID,
+             data.REC_ID,
+             data.VISIT_DATE,
+             ROW_NUMBER() OVER (PARTITION BY CENTRAL_ID ORDER BY VISIT_DATE) AS VISIT_NUM
+      FROM (
+               SELECT CASE
+                          WHEN reg.CENTRAL_ID IS NULL THEN rec.PATIENT_ID
+                          WHEN reg.CENTRAL_ID IS NOT NULL THEN reg.CENTRAL_ID
+                          END AS CENTRAL_ID,
+                      rec.*
+               FROM ohasis_warehouse.form_art_bc rec
+                        LEFT JOIN ohasis_warehouse.id_registry reg ON rec.PATIENT_ID = reg.PATIENT_ID
+           ) AS data) AS artstart
+WHERE VISIT_NUM = 1;
