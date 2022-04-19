10cc585128221c4be13f33768413a555fd8dd577
jrpalo.doh <jrpalo@doh.gov.ph>
2022-04-19 09:38:35 +0800

    added for deletion

--- src/data_warehouse/upsert/form_a.R
+++ src/data_warehouse/upsert/form_a.R
@@ -35,7 +35,19 @@ for_delete_2 <- tbl(lw_conn, dbplyr::in_schema("ohasis_lake", "px_pii")) %>%
    select(REC_ID) %>%
    collect()
 
-for_delete <- bind_rows(for_delete_1, for_delete_2)
+for_delete_3 <- tbl(lw_conn, dbplyr::in_schema("ohasis_lake", "px_pii")) %>%
+   filter(
+      !is.na(DELETED_BY)
+   ) %>%
+   inner_join(
+      y  = tbl(lw_conn, dbplyr::in_schema("ohasis_warehouse", "form_a")) %>%
+         select(REC_ID),
+      by = "REC_ID"
+   ) %>%
+   select(REC_ID) %>%
+   collect()
+
+for_delete <- bind_rows(for_delete_1, for_delete_2, for_delete_3)
 
 # get number of affected rows
 if ((object %>% count() %>% collect())$n > 0) {
