d5f0ef3945ad2ef6fcc06617d111242a23f6e7d4
jrpalo.doh <jrpalo@doh.gov.ph>
2022-05-23 07:39:59 +0800

    updated algorithm for checking of duplicated service

--- src/diagnostics/duped_service.sql
+++ src/diagnostics/duped_service.sql
@@ -3,7 +3,7 @@ FROM ohasis_interim.px_faci
 WHERE REC_ID IN (
     SELECT REC_ID
     FROM ohasis_interim.px_faci
+    WHERE SERVICE_TYPE <> '101102'
     GROUP BY REC_ID
     HAVING COUNT(*) > 1
 )
-  AND SERVICE_TYPE NOT IN ('101102', '101101')
