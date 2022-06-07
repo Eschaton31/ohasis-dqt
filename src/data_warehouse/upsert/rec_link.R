85a7b9d4f981f6fc5a04a9b2ed0b782222e12efd
jrpalo.doh <jrpalo@doh.gov.ph>
2022-06-07 22:29:53 +0800

    added rec_link warehouse

---
+++ src/data_warehouse/upsert/rec_link.R
@@ -0,0 +1,28 @@
+##  OHASIS ID Registry ---------------------------------------------------------
+
+continue <- 0
+id_col   <- c("SOURCE_REC", "DESTINATION_REC")
+# px identifiers (demographics, address, etc.)
+object   <- tbl(db_conn, dbplyr::in_schema("ohasis_interim", "rec_link")) %>%
+   filter(
+      (CREATED_AT >= snapshot_old & CREATED_AT <= snapshot_new) |
+         (UPDATED_AT >= snapshot_old & UPDATED_AT <= snapshot_new) |
+         (DELETED_AT >= snapshot_old & DELETED_AT <= snapshot_new)
+   )
+
+# get number of affected rows
+if ((object %>% count() %>% collect())$n > 0) {
+   continue <- 1
+   object   <- object %>%
+      # keep only form a
+      mutate(
+         SNAPSHOT = case_when(
+            UPDATED_AT > DELETED_AT ~ DELETED_AT,
+            UPDATED_AT < DELETED_AT ~ UPDATED_AT,
+            !is.na(DELETED_AT) ~ DELETED_AT,
+            !is.na(UPDATED_AT) ~ UPDATED_AT,
+            TRUE ~ CREATED_AT
+         )
+      ) %>%
+      collect()
+}
