c2aeba8f11a7474c277cb53a73a566b662ef6a21
John Benedict Palo <jrpalo@doh.gov.ph>
2022-07-06 14:26:59 +0800

    added mailing of ohasis logsheet

--- src/official/dsa_gf/00_main.R
+++ src/official/dsa_gf/00_main.R
@@ -22,134 +22,134 @@ source(file.path(gf$wd, "08_mail.R"))
 try <- gf$logsheet$psfi %>%
    filter(sheet %in% c("MSMTGW", "PWID")) %>%
    left_join(
-      y  = gf$corr$logsheet_psfi$staff %>%
-         rename(
-            site_region = 2
-         ) %>%
-         select(
-            site_region,
-            site_province,
-            site_muncity,
-            ls_subtype,
-            provider_name,
-            PSFI_STAFF_ID,
-            USER_ID,
-            FACI_ID,
-            SUB_FACI_ID
-         ),
-      by = c(
-         "site_region",
-         "site_province",
-         "site_muncity",
-         "ls_subtype",
-         "provider_name"
-      )
+	  y  = gf$corr$logsheet_psfi$staff %>%
+		 rename(
+			site_region = 2
+		 ) %>%
+		 select(
+			site_region,
+			site_province,
+			site_muncity,
+			ls_subtype,
+			provider_name,
+			PSFI_STAFF_ID,
+			USER_ID,
+			FACI_ID,
+			SUB_FACI_ID
+		 ),
+	  by = c(
+		 "site_region",
+		 "site_province",
+		 "site_muncity",
+		 "ls_subtype",
+		 "provider_name"
+	  )
    ) %>%
    mutate(
-      SERVICE_SUB_FACI = case_when(
-         !is.na(FACI_ID) ~ SUB_FACI_ID,
-      ),
-      SERVICE_FACI     = case_when(
-         !is.na(FACI_ID) ~ FACI_ID,
-         !is.na(USER_ID) ~ StrLeft(USER_ID, 6),
-      ),
-      with_uic         = if_else(
-         condition = !is.na(SERVICE_FACI),
-         true      = 1,
-         false     = 0
-      )
+	  SERVICE_SUB_FACI = case_when(
+		 !is.na(FACI_ID) ~ SUB_FACI_ID,
+	  ),
+	  SERVICE_FACI     = case_when(
+		 !is.na(FACI_ID) ~ FACI_ID,
+		 !is.na(USER_ID) ~ StrLeft(USER_ID, 6),
+	  ),
+	  with_uic         = if_else(
+		 condition = !is.na(SERVICE_FACI),
+		 true      = 1,
+		 false     = 0
+	  )
    ) %>%
    left_join(
-      y  = ohasis_uic %>%
-         select(
-            uic = UIC,
-            SERVICE_FACI,
-            PATIENT_ID
-         ) %>%
-         distinct_all(),
-      by = c("uic", "SERVICE_FACI")
+	  y  = ohasis_uic %>%
+		 select(
+			uic = UIC,
+			SERVICE_FACI,
+			PATIENT_ID
+		 ) %>%
+		 distinct_all(),
+	  by = c("uic", "SERVICE_FACI")
    ) %>%
    left_join(
-      y  = ohasis_uic %>%
-         select(
-            uic     = UIC,
-            UIC_PID = PATIENT_ID
-         ) %>%
-         distinct_all(),
-      by = "uic"
+	  y  = ohasis_uic %>%
+		 select(
+			uic     = UIC,
+			UIC_PID = PATIENT_ID
+		 ) %>%
+		 distinct_all(),
+	  by = "uic"
    ) %>%
    mutate(
-      with_pid   = case_when(
-         !is.na(PATIENT_ID) ~ 1,
-         !is.na(UIC_PID) ~ 1,
-         TRUE ~ 0
-      ),
-      PATIENT_ID = case_when(
-         !is.na(PATIENT_ID) ~ PATIENT_ID,
-         TRUE ~ UIC_PID
-      )
+	  with_pid   = case_when(
+		 !is.na(PATIENT_ID) ~ 1,
+		 !is.na(UIC_PID) ~ 1,
+		 TRUE ~ 0
+	  ),
+	  PATIENT_ID = case_when(
+		 !is.na(PATIENT_ID) ~ PATIENT_ID,
+		 TRUE ~ UIC_PID
+	  )
    ) %>%
    left_join(
-      y  = id_registry, ,
-      by = "PATIENT_ID"
+	  y  = id_registry, ,
+	  by = "PATIENT_ID"
    ) %>%
    distinct(ohasis_record, .keep_all = TRUE) %>%
    mutate(
-      CENTRAL_ID = if_else(
-         condition = is.na(CENTRAL_ID),
-         true      = PATIENT_ID,
-         false     = CENTRAL_ID
-      ),
+	  CENTRAL_ID = if_else(
+		 condition = is.na(CENTRAL_ID),
+		 true      = PATIENT_ID,
+		 false     = CENTRAL_ID
+	  ),
    )
 
 
 combined <- bind_rows(
    gf$logsheet$ohasis %>%
-      distinct(ohasis_id, .keep_all = TRUE) %>%
-      anti_join(
-         y  = try %>%
-            select(ohasis_id = CENTRAL_ID),
-         by = "ohasis_id"
-      ) %>%
-      mutate(data_src = "OHASIS") %>%
-      mutate_at(
-         .vars = vars(num_sex_partner_m, num_sex_partner_f),
-         ~as.character(.)
-      ),
+	  distinct(ohasis_id, .keep_all = TRUE) %>%
+	  anti_join(
+		 y  = try %>%
+			select(ohasis_id = CENTRAL_ID),
+		 by = "ohasis_id"
+	  ) %>%
+	  mutate(data_src = "OHASIS") %>%
+	  mutate_at(
+		 .vars = vars(num_sex_partner_m, num_sex_partner_f),
+		 ~as.character(.)
+	  ),
    try %>%
-      anti_join(
-         y  = gf$logsheet$ohasis %>%
-            distinct(ohasis_id, .keep_all = TRUE) %>%
-            select(CENTRAL_ID = ohasis_id),
-         by = "CENTRAL_ID"
-      ) %>%
-      rename(ohasis_id = CENTRAL_ID) %>%
-      mutate(data_src = "PSFI"),
+	  anti_join(
+		 y  = gf$logsheet$ohasis %>%
+			distinct(ohasis_id, .keep_all = TRUE) %>%
+			select(CENTRAL_ID = ohasis_id),
+		 by = "CENTRAL_ID"
+	  ) %>%
+	  rename(ohasis_id = CENTRAL_ID) %>%
+	  mutate(data_src = "PSFI"),
    try %>%
-      inner_join(
-         y  = gf$logsheet$ohasis %>%
-            distinct(ohasis_id, .keep_all = TRUE) %>%
-            select(CENTRAL_ID = ohasis_id),
-         by = "CENTRAL_ID"
-      ) %>%
-      rename(ohasis_id = CENTRAL_ID) %>%
-      mutate(data_src = "Both")
+	  inner_join(
+		 y  = gf$logsheet$ohasis %>%
+			distinct(ohasis_id, .keep_all = TRUE) %>%
+			select(CENTRAL_ID = ohasis_id),
+		 by = "CENTRAL_ID"
+	  ) %>%
+	  rename(ohasis_id = CENTRAL_ID) %>%
+	  mutate(data_src = "Both")
 )
 
 combined <- bind_rows(
    gf$logsheet$ohasis %>%
-      distinct(ohasis_id, .keep_all = TRUE) %>%
-      anti_join(
-         y  = try %>%
-            select(ohasis_id = CENTRAL_ID),
-         by = "ohasis_id"
-      )
+	  distinct(ohasis_id, .keep_all = TRUE) %>%
+	  anti_join(
+		 y  = try %>%
+			select(ohasis_id = CENTRAL_ID),
+		 by = "ohasis_id"
+	  )
 )
 
 faci_users <- ohasis$ref_staff %>%
    mutate(
-      FACI_ID     = StrLeft(STAFF_ID, 6),
-      SUB_FACI_ID = NA_character_
+	  FACI_ID     = StrLeft(STAFF_ID, 6),
+	  SUB_FACI_ID = NA_character_
    )
 
 faci_users <- ohasis$get_faci(
@@ -160,42 +160,42 @@ faci_users <- ohasis$get_faci(
 )
 View(
    faci_users %>%
-      select(
-         STAFF_ID,
-         STAFF_NAME,
-         starts_with("site")
-      ) %>%
-      mutate_all(~toupper(.)) %>%
-      distinct_all(),
+	  select(
+		 STAFF_ID,
+		 STAFF_NAME,
+		 starts_with("site")
+	  ) %>%
+	  mutate_all(~toupper(.)) %>%
+	  distinct_all(),
    "users"
 )
 View(
    ohasis$ref_faci %>%
-      select(FACI_NAME, FACI_ID, contains("NHSSS")),
+	  select(FACI_NAME, FACI_ID, contains("NHSSS")),
    "faci"
 )
 
 psfi_staff <- read_sheet("1qR9sp9VjwGO23vVEr4rMukX6jhDilCa3dbAxcsm2eFI", sheet = "psfi_reference") %>%
    mutate(Region = as.character(Region),
-          Name   = str_squish(toupper(Name)))
+		  Name   = str_squish(toupper(Name)))
 for_match  <- read_sheet("1qR9sp9VjwGO23vVEr4rMukX6jhDilCa3dbAxcsm2eFI", sheet = "staff") %>%
    filter(is.na(USER_ID))
 
 df <- for_match %>%
    mutate(
-      Region = case_when(
-         site_region == "CALABARZON" ~ "4A",
-         site_region == "MIMAROPA" ~ "4B",
-         TRUE ~ stri_replace_all_fixed(site_region, "REGION ", "")
-      )
+	  Region = case_when(
+		 site_region == "CALABARZON" ~ "4A",
+		 site_region == "MIMAROPA" ~ "4B",
+		 TRUE ~ stri_replace_all_fixed(site_region, "REGION ", "")
+	  )
    ) %>%
    rename(Name = provider_name)
 
 df_faci <- faci_users %>%
    select(
-      STAFF_ID,
-      Name = STAFF_NAME,
-      starts_with("site")
+	  STAFF_ID,
+	  Name = STAFF_NAME,
+	  starts_with("site")
    ) %>%
    mutate_all(~toupper(.)) %>%
    distinct_all()
@@ -216,149 +216,149 @@ reclink_df <- fastLink(
 
 if (length(reclink_df$matches$inds.a) > 0) {
    reclink_matched <- getMatches(
-      dfA         = df,
-      dfB         = df_faci,
-      fl.out      = reclink_df,
-      combine.dfs = FALSE
+	  dfA         = df,
+	  dfB         = df_faci,
+	  fl.out      = reclink_df,
+	  combine.dfs = FALSE
    )
 
    reclink_review <- reclink_matched$dfA.match %>%
-      mutate(
-         MATCH_ID = row_number()
-      ) %>%
-      select(
-         MATCH_ID,
-         row_id,
-         Name,
-         site_muncity,
-         site_province,
-         posterior
-      ) %>%
-      left_join(
-         y  = reclink_matched$dfB.match %>%
-            mutate(
-               MATCH_ID = row_number()
-            ) %>%
-            select(
-               MATCH_ID,
-               PSFI_STAFF_ID,
-               PSFI_NAME = Name,
-               Region,
-               `Deployment Facility`,
-               posterior
-            ),
-         by = "MATCH_ID"
-      ) %>%
-      select(-posterior.y) %>%
-      rename(posterior = posterior.x) %>%
-      arrange(desc(posterior)) %>%
-      relocate(posterior, .before = MATCH_ID) %>%
-      # Additional sift through of matches
-      mutate(
-         # levenshtein
-         LV       = stringdist::stringsim(Name, PSFI_NAME, method = 'lv'),
-         # jaro-winkler
-         JW       = stringdist::stringsim(Name, PSFI_NAME, method = 'jw'),
-         # qgram
-         QGRAM    = stringdist::stringsim(Name, PSFI_NAME, method = 'qgram', q = 3),
-         AVG_DIST = (LV + QGRAM + JW) / 3,
-      ) %>%
-      # choose 60% and above match
-      filter(AVG_DIST >= 0.60, !is.na(posterior))
+	  mutate(
+		 MATCH_ID = row_number()
+	  ) %>%
+	  select(
+		 MATCH_ID,
+		 row_id,
+		 Name,
+		 site_muncity,
+		 site_province,
+		 posterior
+	  ) %>%
+	  left_join(
+		 y  = reclink_matched$dfB.match %>%
+			mutate(
+			   MATCH_ID = row_number()
+			) %>%
+			select(
+			   MATCH_ID,
+			   PSFI_STAFF_ID,
+			   PSFI_NAME = Name,
+			   Region,
+			   `Deployment Facility`,
+			   posterior
+			),
+		 by = "MATCH_ID"
+	  ) %>%
+	  select(-posterior.y) %>%
+	  rename(posterior = posterior.x) %>%
+	  arrange(desc(posterior)) %>%
+	  relocate(posterior, .before = MATCH_ID) %>%
+	  # Additional sift through of matches
+	  mutate(
+		 # levenshtein
+		 LV       = stringdist::stringsim(Name, PSFI_NAME, method = 'lv'),
+		 # jaro-winkler
+		 JW       = stringdist::stringsim(Name, PSFI_NAME, method = 'jw'),
+		 # qgram
+		 QGRAM    = stringdist::stringsim(Name, PSFI_NAME, method = 'qgram', q = 3),
+		 AVG_DIST = (LV + QGRAM + JW) / 3,
+	  ) %>%
+	  # choose 60% and above match
+	  filter(AVG_DIST >= 0.60, !is.na(posterior))
 
 }
 
 forma_rr <- gf$logsheet$ohasis %>%
    filter(
-      sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
-      doh_eb_form == "DOH-EB Form: Form A"
+	  sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
+	  doh_eb_form == "DOH-EB Form: Form A"
    ) %>%
    mutate(
-      is_tested = case_when(
-         reactive == "Y" ~ 1,
-         !is.na(test_date) ~ 1,
-         TRUE ~ 0
-      ),
-      reactive  = case_when(
-         is_tested == 1 & reactive == "Y" ~ 1,
-         TRUE ~ 0
-      ),
+	  is_tested = case_when(
+		 reactive == "Y" ~ 1,
+		 !is.na(test_date) ~ 1,
+		 TRUE ~ 0
+	  ),
+	  reactive  = case_when(
+		 is_tested == 1 & reactive == "Y" ~ 1,
+		 TRUE ~ 0
+	  ),
    ) %>%
    group_by(site_name, kap_type) %>%
    summarise(
-      reactive = sum(reactive),
-      tested   = sum(is_tested),
+	  reactive = sum(reactive),
+	  tested   = sum(is_tested),
    ) %>%
    ungroup() %>%
    mutate(rr = format((reactive / tested) * 100, digits = 2))
 
 hts_rr <- gf$logsheet$ohasis %>%
    filter(
-      sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
-      doh_eb_form == "DOH-EB Form: HTS Form"
+	  sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
+	  doh_eb_form == "DOH-EB Form: HTS Form"
    ) %>%
    mutate(
-      is_tested = case_when(
-         reactive == "Y" ~ 1,
-         !is.na(test_date) ~ 1,
-         TRUE ~ 0
-      ),
-      reactive  = case_when(
-         is_tested == 1 & reactive == "Y" ~ 1,
-         TRUE ~ 0
-      ),
+	  is_tested = case_when(
+		 reactive == "Y" ~ 1,
+		 !is.na(test_date) ~ 1,
+		 TRUE ~ 0
+	  ),
+	  reactive  = case_when(
+		 is_tested == 1 & reactive == "Y" ~ 1,
+		 TRUE ~ 0
+	  ),
    ) %>%
    group_by(site_name, kap_type) %>%
    summarise(
-      reactive = sum(reactive),
-      tested   = sum(is_tested),
+	  reactive = sum(reactive),
+	  tested   = sum(is_tested),
    ) %>%
    ungroup() %>%
    mutate(rr = format((reactive / tested) * 100, digits = 2))
 
 cfbs_rr <- gf$logsheet$ohasis %>%
    filter(
-      sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
-      doh_eb_form == "DOH-EB Form: CFBS Form"
+	  sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
+	  doh_eb_form == "DOH-EB Form: CFBS Form"
    ) %>%
    mutate(
-      is_tested = case_when(
-         reactive == "Y" ~ 1,
-         !is.na(test_date) ~ 1,
-         TRUE ~ 0
-      ),
-      reactive  = case_when(
-         is_tested == 1 & reactive == "Y" ~ 1,
-         TRUE ~ 0
-      ),
+	  is_tested = case_when(
+		 reactive == "Y" ~ 1,
+		 !is.na(test_date) ~ 1,
+		 TRUE ~ 0
+	  ),
+	  reactive  = case_when(
+		 is_tested == 1 & reactive == "Y" ~ 1,
+		 TRUE ~ 0
+	  ),
    ) %>%
    group_by(site_name, kap_type) %>%
    summarise(
-      reactive = sum(reactive),
-      tested   = sum(is_tested),
+	  reactive = sum(reactive),
+	  tested   = sum(is_tested),
    ) %>%
    ungroup() %>%
    mutate(rr = format((reactive / tested) * 100, digits = 2))
 
 oh_rr <- gf$logsheet$ohasis %>%
    filter(
-      sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
+	  sextype_anal_insert == "Y" | sextype_anal_receive == "Y",
    ) %>%
    mutate(
-      is_tested = case_when(
-         reactive == "Y" ~ 1,
-         !is.na(test_date) ~ 1,
-         TRUE ~ 0
-      ),
-      reactive  = case_when(
-         is_tested == 1 & reactive == "Y" ~ 1,
-         TRUE ~ 0
-      ),
+	  is_tested = case_when(
+		 reactive == "Y" ~ 1,
+		 !is.na(test_date) ~ 1,
+		 TRUE ~ 0
+	  ),
+	  reactive  = case_when(
+		 is_tested == 1 & reactive == "Y" ~ 1,
+		 TRUE ~ 0
+	  ),
    ) %>%
    group_by(site_name, kap_type) %>%
    summarise(
-      reactive = sum(reactive),
-      tested   = sum(is_tested),
+	  reactive = sum(reactive),
+	  tested   = sum(is_tested),
    ) %>%
    ungroup() %>%
    mutate(rr = format((reactive / tested) * 100, digits = 2))
@@ -367,48 +367,70 @@ write_xlsx(list("CBS RR" = cfbs_rr, "Form A RR" = forma_rr, "HTS Form RR" = hts_
 
 combined %>%
    mutate(
-      is_tested = case_when(
-         reactive == "Y" ~ 1,
-         !is.na(test_date) ~ 1,
-         TRUE ~ 0
-      ),
-      reactive  = case_when(
-         is_tested == 1 & reactive == "Y" ~ 1,
-         TRUE ~ 0
-      ),
+	  is_tested = case_when(
+		 reactive == "Y" ~ 1,
+		 !is.na(test_date) ~ 1,
+		 TRUE ~ 0
+	  ),
+	  reactive  = case_when(
+		 is_tested == 1 & reactive == "Y" ~ 1,
+		 TRUE ~ 0
+	  ),
    ) %>%
    group_by(kap_type, data_src) %>%
    summarise(
-      reactive = sum(reactive),
-      tested   = sum(is_tested),
+	  reactive = sum(reactive),
+	  tested   = sum(is_tested),
    ) %>%
    ungroup()
 
 write_xlsx(
    combined %>%
-      mutate(
-         is_tested = case_when(
-            reactive == "Y" ~ 1,
-            !is.na(test_date) ~ 1,
-            TRUE ~ 0
-         ),
-         reactive  = case_when(
-            is_tested == 1 & reactive == "Y" ~ 1,
-            TRUE ~ 0
-         ),
-      ) %>%
-      group_by(kap_type, data_src) %>%
-      summarise(
-         reactive = sum(reactive),
-         tested   = sum(is_tested),
-      ) %>%
-      ungroup() %>%
-      mutate(rr = format((reactive / tested) * 100, digits = 2)),
+	  mutate(
+		 is_tested = case_when(
+			reactive == "Y" ~ 1,
+			!is.na(test_date) ~ 1,
+			TRUE ~ 0
+		 ),
+		 reactive  = case_when(
+			is_tested == 1 & reactive == "Y" ~ 1,
+			TRUE ~ 0
+		 ),
+	  ) %>%
+	  group_by(kap_type, data_src) %>%
+	  summarise(
+		 reactive = sum(reactive),
+		 tested   = sum(is_tested),
+	  ) %>%
+	  ungroup() %>%
+	  mutate(rr = format((reactive / tested) * 100, digits = 2)),
    "H:/20220520_rr-by-kap_src.xlsx"
 )
 
 .tab(
    combined %>%
-      filter(sextype_anal_insert == "Y" | sextype_anal_receive == "Y"),
+	  filter(sextype_anal_insert == "Y" | sextype_anal_receive == "Y"),
    reactive
+)
+
+write_dta(
+   gf$logsheet$ohasis %>%
+	  filter(with_dsa == 1) %>%
+	  select(
+		 -starts_with("RISK"),
+		 -starts_with("EXPOSE"),
+		 RECORD_P12M
+	  ),
+   "H:/Data Sharing/PSFI/2022.06/ohasis_logsheet_2022-06.dta"
+)
+
+write_xlsx(
+   gf$logsheet$ohasis %>%
+	  filter(with_dsa == 1) %>%
+	  select(
+		 -starts_with("RISK"),
+		 -starts_with("EXPOSE"),
+		 RECORD_P12M
+	  ),
+   "H:/Data Sharing/PSFI/2022.06/ohasis_logsheet_2022-06.xlsx"
 )
--- src/official/dsa_gf/08_mail.R
+++ src/official/dsa_gf/08_mail.R
@@ -1,3 +1,5 @@
+##  Aggregates -----------------------------------------------------------------
+
 gf$as_of <- as.POSIXct(
    paste0(
       strsplit(ohasis$timestamp, "\\.")[[1]][1], "-",
@@ -96,3 +98,106 @@ gf$mail$for_release <- gm_mime() %>%
       )
    ) %>%
    gm_attach_file(file.path(gf$xlsx$dir, gf$xlsx$file))
+
+##  Logsheet -----------------------------------------------------------------
+
+gf$as_of <- as.POSIXct(
+   paste0(
+      strsplit(ohasis$timestamp, "\\.")[[1]][1], "-",
+      strsplit(ohasis$timestamp, "\\.")[[1]][2], "-",
+      strsplit(ohasis$timestamp, "\\.")[[1]][3], " ",
+      StrLeft(strsplit(ohasis$timestamp, "\\.")[[1]][4], 2), ":",
+      substr(strsplit(ohasis$timestamp, "\\.")[[1]][4], 3, 4), ":",
+      StrRight(strsplit(ohasis$timestamp, "\\.")[[1]][4], 2), ":"
+   )
+)
+
+gf$mail$title <- glue("PSFI-PROTECTS Logsheets for GF-Supported Sites w/ DSA ({gf$coverage$curr_yr}-{gf$coverage$curr_mo})")
+
+gf$mail$to <- c(
+   "crmaranan@pilipinasshellfoundation.org",
+   "lbpnorella@pilipinasshellfoundation.org",
+   "agdelacruz@pilipinasshellfoundation.org",
+   "me.hiv@pilipinasshellfoundation.org"
+)
+
+gf$mail$for_checking <- gm_mime() %>%
+   gm_to(
+      unique(
+         c(
+            gmail$nhsss$ss,
+            gmail$nhsss$dqt,
+            gmail$nhsss$dat
+         )
+      )
+   ) %>%
+   gm_from(Sys.getenv("GMAIL_USER")) %>%
+   gm_subject(glue("[For Checking] {gf$mail$title}")) %>%
+   gm_html_body(
+      glue(
+         r"(
+<p>Good day,</p>
+<p>You are receiving this message because you are a member of one of the HIV surveillance systems utilized in generating the PSFI-PROTECTS Project Indicators.
+<p>This communication is specifically for the monthly indicators and the required counter-checking and validation to be conducted prior to releasing to the GF M&E Team.</p>
+<p>For the staff new to the MER Indicators CC, reach out to your seniors to provide context on what is to be done.</p>
+<p>Attached are the files pertaining to the subject above. Kindly conduct the necessary checking. Final release is slated to be before COB today, May 10.</p>
+{gmail_sig()}
+         )"
+      )
+   ) %>%
+   gm_attach_file(file.path(gf$xlsx$dir, gf$xlsx$file))
+
+gf$mail$for_approval <- gm_mime() %>%
+   gm_to(gmail$nhsss$head) %>%
+   gm_cc(
+      unique(
+         c(
+            gmail$nhsss$ss,
+            gmail$nhsss$dqt,
+            gmail$nhsss$dat
+         )
+      )
+   ) %>%
+   gm_from(Sys.getenv("GMAIL_USER")) %>%
+   gm_subject(glue("[For Approval] {gf$mail$title}")) %>%
+   gm_html_body(
+      glue(
+         r"(
+<p>Hi Sir Noel & Ate Ja,</p>
+<p>Attached here are the logsheet files set to be released to the GF M&E Team. All data were prepared by myself. Data has been limited to those who have submitted a DSA between their faciity and PSFI.</p>
+<p>Once you provide approval, I will release ASAP to the GF M&E Team.</p>
+{gmail_sig()}
+         )"
+      )
+   ) %>%
+   gm_attach_file("H:/Data Sharing/PSFI/2022.06/ohasis_logsheet_2022-06.xlsx") %>%
+   gm_attach_file("H:/Data Sharing/PSFI/2022.06/ohasis_logsheet_2022-06.dta")
+
+gf$mail$for_release <- gm_mime() %>%
+   gm_to(
+      gf$mail$to
+   ) %>%
+   gm_cc(
+      unique(
+         c(
+            gmail$nhsss$head,
+            gmail$nhsss$ss,
+            gmail$nhsss$dqt,
+            gmail$nhsss$dat
+         )
+      )
+   ) %>%
+   gm_from(Sys.getenv("GMAIL_USER")) %>%
+   gm_subject(glue("[Data Release] {gf$mail$title}")) %>%
+   gm_html_body(
+      glue(
+         r"(
+<p>Hi GF-PROTECTS M&E Team,</p>
+<p>Attached here are the monthly PSFI-PROTECTS Logsheet data for GF-Supported Sites w/ DSA as of <b>{month.name[as.numeric(gf$coverage$curr_mo)]} {gf$coverage$curr_yr}</b>. OHASIS data was extracted as of <b>{format(gf$as_of, "%a %b %d, %Y %X")}</b>.</p>
+<p>Should you have any questions or clarifications, please don\'t hesitate to let me know.</p>
+{gmail_sig()}
+         )"
+      )
+   )%>%
+   gm_attach_file("H:/Data Sharing/PSFI/2022.06/ohasis_logsheet_2022-06.xlsx") %>%
+   gm_attach_file("H:/Data Sharing/PSFI/2022.06/ohasis_logsheet_2022-06.dta")
