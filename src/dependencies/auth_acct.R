610f217d2b44e803600f59cb22f62fd1a608012f
jrpalo.doh <jrpalo@doh.gov.ph>
2022-07-21 08:21:06 +0800

    resolved issue with expiring dropbox token

--- src/dependencies/auth_acct.R
+++ src/dependencies/auth_acct.R
@@ -10,11 +10,23 @@ gs4_auth(cache = ".secrets")
 
 # Dropbox
 # trigger auth on purpose --> store a token in the specified cache
-if (!file.exists(".secrets/hivregistry.nec@gmail.com.RDS")) {
+if (!file.exists(".secrets/hivregistry.nec@gmail.com.RDS") & !is.null(drop_acc()$error_summary)) {
+   options(browser = Sys.getenv("BROWSER"))
    token <- drop_auth(new_user = TRUE)
    saveRDS(token, ".secrets/hivregistry.nec@gmail.com.RDS")
    rm("token")
-} else {
+   options(
+      browser = function(url) {
+         if (grepl('^https?:', url)) {
+            if (!.Call('.jetbrains_processBrowseURL', url)) {
+               browseURL(url, .jetbrains$ther_old_browser)
+            }
+         } else {
+            .Call('.jetbrains_showFile', url, url)
+         }
+      }
+   )
+} else if (file.exists(".secrets/hivregistry.nec@gmail.com.RDS") & !is.null(drop_acc()$error_summary)) {
    token <- readRDS(".secrets/hivregistry.nec@gmail.com.RDS")
    drop_acc(dtoken = token)
    rm("token")
