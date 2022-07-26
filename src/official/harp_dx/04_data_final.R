960281c221e2b8fa3deedb42dae9a549064a7cf0
John Benedict Palo <jrpalo@doh.gov.ph>
2022-07-26 12:53:10 +0800

    fixed dropping of clients in data_final of harp_dx

--- src/official/harp_dx/04_data_final.R
+++ src/official/harp_dx/04_data_final.R
@@ -9,8 +9,8 @@ nhsss$harp_dx$official$new <- nhsss$harp_dx$official$old %>%
    bind_rows(nhsss$harp_dx$converted$data) %>%
    arrange(idnum) %>%
    mutate(
-      drop_notyet     = 0,
-      drop_duplicates = 0,
+	  drop_notyet     = 0,
+	  drop_duplicates = 0,
    )
 
 ##  Tag data to be reported later on and duplicates for dropping ---------------
@@ -18,11 +18,22 @@ nhsss$harp_dx$official$new <- nhsss$harp_dx$official$old %>%
 .log_info("Tagging duplicates and postponed reports.")
 for (drop_var in c("drop_notyet", "drop_duplicates"))
    if (drop_var %in% names(nhsss$harp_dx$corr))
-      nhsss$harp_dx$official$new %<>%
-         anti_join(
-            y  = nhsss$harp_dx$corr[[drop_var]] %>% select(idnum = idnum_drop),
-            by = "idnum"
-         )
+	  nhsss$harp_dx$official$new %<>%
+		 left_join(
+			y = nhsss$harp_dx$corr[[drop_var]] %>%
+			   select(REC_ID) %>%,
+			mutate(drop = 1)
+			by = "REC_ID"
+		 ) %>%
+		 mutate(
+			drop_var := if_else(
+			   condition = drop == 1,
+			   true      = 1,
+			   false     = !!drop_var,
+			   missing   = !!drop_var
+			)
+		 ) %>%
+		 select(-drop)
 
 ##  Subsets for documentation --------------------------------------------------
 
@@ -38,14 +49,14 @@ nhsss$harp_dx$official$dropped_duplicates <- nhsss$harp_dx$official$new %>%
 .log_info("Finalizing dataset.")
 nhsss$harp_dx$official$new %<>%
    mutate(
-      labcode2    = if_else(
-         condition = is.na(labcode2),
-         true      = labcode,
-         false     = labcode2,
-         missing   = labcode2
-      ),
-      drop        = drop_duplicates + drop_notyet,
-      who_staging = as.integer(who_staging)
+	  labcode2    = if_else(
+		 condition = is.na(labcode2),
+		 true      = labcode,
+		 false     = labcode2,
+		 missing   = labcode2
+	  ),
+	  drop        = drop_duplicates + drop_notyet,
+	  who_staging = as.integer(who_staging)
    ) %>%
    filter(drop == 0) %>%
    select(-drop, -drop_duplicates, -drop_notyet, -mot)
