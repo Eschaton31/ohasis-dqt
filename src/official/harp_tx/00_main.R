e9f900e8906573ad4d723dca5d08ebf16fb43028
John Benedict Palo <jrpalo@doh.gov.ph>
2022-07-29 11:29:27 +0800

    new dedup_by sheet

--- src/official/harp_tx/00_main.R
+++ src/official/harp_tx/00_main.R
@@ -55,10 +55,10 @@ if (complete == "1") {
 rm(complete)
 
 dedup_by(
-   "1vmPRZCKd7kzcCZ8DdiORDbD-rKem-4rGIJKF2-125lY",
+   "1mLlIrYAqBXQJ2Bc3gNQH0l0JxfmfBBtLGaO5I4hpovY",
    "reclink",
    23,
    26,
    1,
-   1046
+   1978
 )
