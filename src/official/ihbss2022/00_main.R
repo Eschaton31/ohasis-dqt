96134c7c2de5fefc8def69bf8eab14d3b4418cb4
jrpalo.doh <jrpalo@doh.gov.ph>
2022-08-28 01:26:17 +0800

    initial ihbss2022 conso

--- src/official/ihbss2022/00_main.R
+++ src/official/ihbss2022/00_main.R
@@ -3,26 +3,65 @@
 if (!exists("ihbss"))
    ihbss <- new.env()
 
-ihbss$`2022` <- new.env()
+ihbss$`2022`            <- new.env()
+ihbss$`2022`$odk$config <- read_sheet("1SNB43JjGOB-uEivT5n8bfGpxiTEKkhNM7izo-KCQKPY", "forms")
 
 # setup
-ruODK::ru_setup(
-   svc     = odk_test,
-   tz      = "Asia/Hong_Kong",
-   verbose = TRUE
-)
+ihbss$`2022`$odk$submissions <- list()
+ihbss$`2022`$odk$data        <- list()
+for (i in seq_len(nrow(ihbss$`2022`$odk$config))) {
+   config <- ihbss$`2022`$odk$config[i,]
+   form   <- paste(sep = " - ", config$City, config$Language)
+
+   ruODK::ru_setup(
+      svc     = config$OData,
+      tz      = "Asia/Hong_Kong",
+      verbose = FALSE
+   )
+
+   check_dir(file.path("H:/_R/library/ihbss2022", form))
+   tryCatch({
+      ihbss$`2022`$odk$submissions[[form]] <- ruODK::submission_list()
+   },
+      error = function(e) .log_info("{e}")
+   )
+
+   ihbss$`2022`$odk$data[[form]] <- ruODK::odata_submission_get(download = TRUE, local_dir = file.path("H:/_R/library/ihbss2022", form)) %>%
+      mutate(Form = form)
+}
 
 # list projects & forms
-ihbss$`2022`$odk$projects    <- ruODK::project_list()
-ihbss$`2022`$odk$forms       <- ruODK::form_list(pid = 7)
-ihbss$`2022`$odk$submissions <- ruODK::submission_list(
-   pid = 7,
-   fid = ihbss$`2022`$odk$forms$fid
-)
-ihbss$`2022`$odk$data        <- ruODK::odata_submission_get(
-   pid      = 7,
-   fid      = ihbss$`2022`$odk$forms$fid,
-   download = FALSE
-)
+ihbss$`2022`$odk$submissions <- ruODK::submission_list()
+ihbss$`2022`$odk$data        <- ruODK::odata_submission_get(download = TRUE, local_dir = file.path(" H:/_R/library/ihbss2022", form))
 
 ruODK::user_list()
+subs <- bind_rows(ihbss$`2022`$odk$submissions)
+data <- bind_rows(ihbss$`2022`$odk$data)
+
+data %>%
+   select(
+      id,
+      d8 = d_8_months_with_anal_sex_act
+   ) %>%
+   separate(
+      d8,
+      sep  = " ",
+      into = c("m01", "m02", "m03", "m04", "m05", "m06", "m07", "m08", "m09", "m10", "m11", "m12")
+   ) %>%
+   pivot_longer(
+      cols      = starts_with("m"),
+      names_to  = "num",
+      values_to = "month"
+   ) %>%
+   filter(!is.na(month)) %>%
+   arrange(month) %>%
+   mutate(
+      month = month.abb[as.numeric(month)],
+      num   = 1,
+   ) %>%
+   pivot_wider(
+      id_cols      = id,
+      names_from   = month,
+      values_from  = num,
+      names_prefix = "d_8_"
+   )
