87098a7d33f7d4d0a8ec7188094bc3a30d90953e
jrpalo.doh <jrpalo@doh.gov.ph>
2022-08-28 09:51:16 +0800

    added function to format stata .dta

--- src/dependencies/pipeline.R
+++ src/dependencies/pipeline.R
@@ -45,3 +45,30 @@ pipeline <- function(system, parent, step = NULL, group = "official") {
    # re-assign w/ changes
    .GlobalEnv[[p_name]][[s_name]] <- envir
 }
+
+# format stata exports
+format_stata <- function(data) {
+   # convert to integers
+   vars <- colnames(select_if(data, .predicate = is.numeric))
+   for (var in vars) {
+      test_int <- any(data[[var]] %% 1 != 0)
+      test_int <- ifelse(is.na(test_int), FALSE, test_int)
+      if (test_int == TRUE) storage.mode(data[[var]]) <- "integer"
+   }
+
+   # format dates
+   vars <- colnames(select_if(data, .predicate = is.Date))
+   for (var in vars) {
+      storage.mode(data[[var]])            <- "integer"
+      attributes(data[[var]])$format.stata <- "%tdCCYY-NN-DD"
+   }
+
+   # format strings
+   vars <- colnames(select_if(data, .predicate = is.character))
+   for (var in vars) {
+      char_fmt <- max(nchar(data[[var]]), na.rm = TRUE)
+      attributes(data[[var]])$format.stata <- paste0("%-", char_fmt, "s")
+   }
+
+   return(data)
+}
