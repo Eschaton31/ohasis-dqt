fccd2dae0604098f6b1ed20f27a9248d22da2ac4
John Benedict Palo <jrpalo@doh.gov.ph>
2022-12-05 10:53:32 +0800

    added cutoffs to params

--- src/official/harp_tx/01_load_reqs.R
+++ src/official/harp_tx/01_load_reqs.R
@@ -356,6 +356,12 @@ define_params <- function() {
    local(envir = nhsss$harp_dx, {
       params               <- list()
       params$latest_art_id <- max(as.integer(official$old_reg$art_id), na.rm = TRUE)
+
+      params$cutoff_mo   <- if_else(as.numeric(ohasis$next_mo) <= 3, as.numeric(ohasis$next_mo) + 9, as.numeric(ohasis$next_mo) - 3)
+      params$cutoff_mo   <- stri_pad_left(as.character(params$cutoff_mo), 2, '0')
+      params$cutoff_yr   <- if_else(as.numeric(ohasis$next_mo) <= 3, as.numeric(ohasis$next_yr) - 1, as.numeric(ohasis$next_yr))
+      params$cutoff_yr   <- as.character(params$cutoff_yr)
+      params$cutoff_date <- as.Date(paste(sep = '-', params$cutoff_yr, params$cutoff_mo, '01'))
    })
 }
 
