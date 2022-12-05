7fe91c78409d6f140c4aa1d4e22ff79a919d199d
John Benedict Palo <jrpalo@doh.gov.ph>
2022-12-05 12:04:23 +0800

    registered dedup tasks for harp_tx flows

--- src/official/harp_tx/_init.R
+++ src/official/harp_tx/_init.R
@@ -7,7 +7,10 @@ flow(
       "04" = "data_reg.final",
       "05" = "data_outcome.initial",
       "06" = "data_outcome.convert",
-      "07" = "data_outcome.final"
+      "07" = "data_outcome.final",
+      "x1" = "dedup_new",
+      "x2" = "dedup_old",
+      "x3" = "dedup_dx"
    ),
    file.path(getwd(), "src", "official", "harp_tx"),
    nhsss
