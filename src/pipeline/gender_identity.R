0efff005e8fceb9bee6ef7c6a67c216b364d3ff4
John Benedict Palo <jrpalo@doh.gov.ph>
2023-01-03 08:12:54 +0800

    helper function for gender identity

---
+++ src/pipeline/gender_identity.R
@@ -0,0 +1,44 @@
+generate_gender_identity <- function(linelist, sex, self_identity, self_identity_text, gender_identity) {
+   col_sex <- deparse(substitute(sex))
+   col_si  <- deparse(substitute(self_identity))
+   ref_gi  <- read_sheet("1LFYk4FGokmq4OzBsU6KW6cJWPUYKagXvR6VvvWu1uNo", "Sheet1")
+   linelist %<>%
+      # clean data
+      mutate_at(
+         .vars = vars({{sex}}, {{self_identity}}),
+         ~toupper(remove_code(.))
+      ) %>%
+      mutate_at(
+         .vars = vars({{sex}}, {{self_identity}}),
+         ~case_when(
+            . %in% c("MALE", "MAN") ~ "MALE",
+            . %in% c("FEMALE", "WOMAN") ~ "FEMALE",
+            . %in% c("OTHER", "OTHERS") ~ "OTHERS",
+            TRUE ~ .
+         )
+      ) %>%
+      mutate(
+         {{self_identity_text}} := toupper(str_squish({{self_identity_text}})),
+         SI_SIEVE               = if_else(
+            condition = !is.na({{self_identity_text}}),
+            true      = str_replace_all({{self_identity_text}}, "[^[:alnum:]]", ""),
+            false     = NA_character_
+         ),
+      ) %>%
+      # gender identity
+      left_join(
+         y  = ref_gi %>%
+            mutate(SEX = remove_code(SEX)) %>%
+            select(
+               {{sex}}             := SEX,
+               {{self_identity}}   := self_identity,
+               SI_SIEVE            = self_identity_other_sieve,
+               {{gender_identity}} := gender_identity
+            ),
+         by = c(col_sex, col_si, "SI_SIEVE")
+      ) %>%
+      relocate({{gender_identity}}, .before = {{self_identity}}) %>%
+      select(-SI_SIEVE)
+
+   return(linelist)
+}
