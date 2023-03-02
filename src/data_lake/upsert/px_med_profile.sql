8354dabf347a213f4d0d05c59acac6b6e35d76a0
John Benedict Palo <jrpalo@doh.gov.ph>
2023-03-02 08:37:18 +0800

    additional sql files for future building of HTS Data Forms in the warehouse

---
+++ src/data_lake/upsert/px_med_profile.sql
@@ -0,0 +1,27 @@
+SELECT rec.REC_ID,
+       rec.CREATED_AT,
+       rec.UPDATED_AT,
+       rec.DELETED_AT,
+       GREATEST(COALESCE(rec.DELETED_AT, 0), COALESCE(rec.UPDATED_AT, 0), COALESCE(rec.CREATED_AT, 0)) AS SNAPSHOT,
+
+       MAX(IF(med_profile.PROFILE = 1, IS_PROFILE, NULL))                                              AS MED_TB_TX,
+       MAX(IF(med_profile.PROFILE = 2, IS_PROFILE, NULL))                                              AS MED_IS_PREGNANT,
+       MAX(IF(med_profile.PROFILE = 3, IS_PROFILE, NULL))                                              AS MED_HEP_B,
+       MAX(IF(med_profile.PROFILE = 4, IS_PROFILE, NULL))                                              AS MED_HEP_C,
+       MAX(IF(med_profile.PROFILE = 5, IS_PROFILE, NULL))                                              AS MED_CBS_REACTIVE,
+       MAX(IF(med_profile.PROFILE = 6, IS_PROFILE, NULL))                                              AS MED_PREP_PX,
+       MAX(IF(med_profile.PROFILE = 7, IS_PROFILE, NULL))                                              AS MED_PEP_PX,
+       MAX(IF(med_profile.PROFILE = 8, IS_PROFILE, NULL))                                              AS MED_STI
+FROM ohasis_interim.px_record AS rec
+         JOIN (SELECT REC_ID,
+                      PROFILE,
+                      CASE IS_PROFILE
+                          WHEN 1 THEN '1_Yes'
+                          WHEN 0 THEN '0_No'
+                          ELSE NULL END AS IS_PROFILE
+               FROM ohasis_interim.px_med_profile) AS med_profile ON rec.REC_ID = med_profile.REC_ID
+WHERE ((rec.CREATED_AT BETWEEN ? AND ?) OR
+       (rec.UPDATED_AT BETWEEN ? AND ?) OR
+       (rec.DELETED_AT BETWEEN ? AND ?))
+GROUP BY rec.REC_ID;
+-- ID_COLS: REC_ID
---
+++ src/data_lake/upsert/px_test_reason.sql
@@ -0,0 +1,30 @@
+SELECT rec.REC_ID,
+       rec.CREATED_AT,
+       rec.UPDATED_AT,
+       rec.DELETED_AT,
+       GREATEST(COALESCE(rec.DELETED_AT, 0), COALESCE(rec.UPDATED_AT, 0), COALESCE(rec.CREATED_AT, 0)) AS SNAPSHOT,
+
+       MAX(IF(test_reason.REASON = 1, IS_REASON, NULL))                                                AS TEST_REASON_HIV_EXPOSE,
+       MAX(IF(test_reason.REASON = 2, IS_REASON, NULL))                                                AS TEST_REASON_PHYSICIAN,
+       MAX(IF(test_reason.REASON = 3, IS_REASON, NULL))                                                AS TEST_REASON_EMPLOY_OFW,
+       MAX(IF(test_reason.REASON = 4, IS_REASON, NULL))                                                AS TEST_REASON_EMPLOY_LOCAL,
+       MAX(IF(test_reason.REASON = 5, IS_REASON, NULL))                                                AS TEST_REASON_INSURANCE,
+       MAX(IF(test_reason.REASON = 6, IS_REASON, NULL))                                                AS TEST_REASON_NO_REASON,
+       MAX(IF(test_reason.REASON = 7, IS_REASON, NULL))                                                AS TEST_REASON_RETEST,
+       MAX(IF(test_reason.REASON = 8, IS_REASON, NULL))                                                AS TEST_REASON_PEER_ED,
+       MAX(IF(test_reason.REASON = 9, IS_REASON, NULL))                                                AS TEST_REASON_TEXT_EMAIL,
+       MAX(IF(test_reason.REASON = 8888, REASON_OTHER, NULL))                                          AS TEST_REASON_OTHER_TEXT
+FROM ohasis_interim.px_record AS rec
+         JOIN (SELECT REC_ID,
+                      REASON,
+                      CASE IS_REASON
+                          WHEN 1 THEN '1_Yes'
+                          WHEN 0 THEN '0_No'
+                          ELSE NULL END AS IS_REASON,
+                      REASON_OTHER
+               FROM ohasis_interim.px_test_reason) AS test_reason ON rec.REC_ID = test_reason.REC_ID
+WHERE ((rec.CREATED_AT BETWEEN ? AND ?) OR
+       (rec.UPDATED_AT BETWEEN ? AND ?) OR
+       (rec.DELETED_AT BETWEEN ? AND ?))
+GROUP BY rec.REC_ID;
+-- ID_COLS: REC_ID
