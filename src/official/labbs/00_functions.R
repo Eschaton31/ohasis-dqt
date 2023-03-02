28ed14c2961cb9d0b19d0aa70bae4996a04349ae
John Benedict Palo <jrpalo@doh.gov.ph>
2023-03-02 08:35:42 +0800

    updated data processing for labbs consolidation sheet; mostly final, waiting on addition of validation sheets and facility ids

--- src/official/labbs/00_functions.R
+++ src/official/labbs/00_functions.R
@@ -1,4 +1,4 @@
-labbs_sheet <- function(xl, disease, sheet_name) {
+labbs_sheet <- function(xl, disease, sheet_name, env = globalenv()) {
    # extract region from filename
    # i.e., 20230222_labbs_region-NCR_2022-12.xlsx
    filename <- strsplit(basename(xl), "_")[[1]]
@@ -10,6 +10,7 @@ labbs_sheet <- function(xl, disease, sheet_name) {
       # read excel file into a dataframe, copy over data for merged cells
       # NOTE: Do not use the first row as headers
       data <- read.xlsx(xl, sheet = sheet_name, fillMergedCells = TRUE, skipEmptyCols = FALSE, skipEmptyRows = FALSE, colNames = FALSE) %>%
+         remove_empty("cols") %>%
          as_tibble() %>%
          mutate(row_id = row_number())
 
@@ -30,19 +31,19 @@ labbs_sheet <- function(xl, disease, sheet_name) {
       # type of facility was included as some regions were noted to have deleted
       # the "Name of Facility" column
       col_of_name <- NA
-      if ("NAME OF FACILITY" %in% val_col1) col_of_name <- 1
-      if ("NAME OF FACILITY" %in% val_col2) col_of_name <- 2
-      if ("NAME OF FACILITY" %in% val_col3) col_of_name <- 3
+      if ("NAME OF FACILITY" %in% val_col1 && is.na(col_of_name)) col_of_name <- 1
+      if ("NAME OF FACILITY" %in% val_col2 && is.na(col_of_name)) col_of_name <- 2
+      if ("NAME OF FACILITY" %in% val_col3 && is.na(col_of_name)) col_of_name <- 3
 
       col_of_type <- NA
-      if ("TYPE OF FACILITY" %in% val_col1) col_of_type <- 1
-      if ("TYPE OF FACILITY" %in% val_col2) col_of_type <- 2
-      if ("TYPE OF FACILITY" %in% val_col3) col_of_type <- 3
+      if ("TYPE OF FACILITY" %in% val_col1 && is.na(col_of_type)) col_of_type <- 1
+      if ("TYPE OF FACILITY" %in% val_col2 && is.na(col_of_type)) col_of_type <- 2
+      if ("TYPE OF FACILITY" %in% val_col3 && is.na(col_of_type)) col_of_type <- 3
 
       col_of_bb <- NA
-      if ("NAME OF BLOODBANK" %in% val_col1) col_of_bb <- 1
-      if ("NAME OF BLOODBANK" %in% val_col2) col_of_bb <- 2
-      if ("NAME OF BLOODBANK" %in% val_col3) col_of_bb <- 3
+      if ("NAME OF BLOODBANK" %in% val_col1 && is.na(col_of_bb)) col_of_bb <- 1
+      if ("NAME OF BLOODBANK" %in% val_col2 && is.na(col_of_bb)) col_of_bb <- 2
+      if ("NAME OF BLOODBANK" %in% val_col3 && is.na(col_of_bb)) col_of_bb <- 3
 
       col_faci_name <- coalesce(col_of_bb, col_of_name, col_of_type)
 
@@ -59,31 +60,51 @@ labbs_sheet <- function(xl, disease, sheet_name) {
       # tag which version of the labbs form they are using;
       # > 4 rows = old
       # > 3 rows = new
-      form_version <- as.character(data[3, 3])
-      if (form_version == "*Please indicate the corresponding number")
+      form_version_1 <- coalesce(as.character(data[3, 3]), "")
+      form_version_2 <- coalesce(as.character(data[4, 3]), "")
+      if (form_version_1 == "*Please indicate the corresponding number" | form_version_2 == "*Please indicate the corresponding number")
          data %<>%
             filter(row_number() != 3)
+
+      # tag those who added an extra row after the m, f, toal
+      f_col09 <- coalesce(as.character(data[3, 9]), "")
+      f_col10 <- coalesce(as.character(data[3, 10]), "")
+      if (f_col09 == "F" & f_col10 == "F")
+         data %<>%
+            filter(row_number() != 3)
+
+      if (region == "NCR-bb")
+         data %<>%
+            add_row(X2 = NA_character_, .after = 2)
 
       # extract names from each row
       row1 <- toupper(as.character(str_squish(data[1,])))
       row2 <- toupper(as.character(str_squish(data[2,])))
       row3 <- toupper(as.character(str_squish(data[3,])))
 
+      # check weird unmerging of old forms
+      for (i in seq_len(length(row3) - 1))
+         row3[i] <- ifelse(is.na(row3[i]) & row3[i + 1] == "TOTAL F", "M", row3[i])
+
       # clean the names to be used; convert to snake-case
       row1 <- case_when(
          row1 == "NAME OF FACILITY" ~ "faci_name",
+         row2 == "NAME OF BLOODBANK" ~ "faci_name",
          row1 == "TYPE OF FACILITY" ~ "faci_type",
          row1 == "OWNERSHIP" ~ "ownership",
+         row1 == "OWNERHSIP" ~ "ownership",
          row1 == "MUN/CITY" ~ "muncity",
+         row2 == "MUNCITY" ~ "muncity",
          row1 == "PROVINCE" ~ "province",
          row1 == "REMARKS" ~ "remarks",
+         grepl("^1- PRIVATE HOSPITAL", row2) ~ "faci_type",
          !is.na(match(row1, toupper(month.name))) ~ stri_pad_left(match(row1, toupper(month.name)), 2, "0"),
          TRUE ~ row1
       )
 
       # read the column renaming from labbs_config, limit to the columns for the
       # current sheet
-      sheet_cols <- filter(labbs$config$cols, sheet == disease)
+      sheet_cols <- filter(env$config$cols, sheet == disease)
       for (i in seq_len(nrow(sheet_cols))) {
          xl_col <- as.character(sheet_cols[i, "xl_col"])
          r_col  <- as.character(sheet_cols[i, "r_col"])
@@ -91,15 +112,17 @@ labbs_sheet <- function(xl, disease, sheet_name) {
       }
       row2 <- case_when(
          row2 == "NAME OF FACILITY" ~ "faci_name",
+         row2 == "NAME OF BLOODBANK" ~ "faci_name",
          row2 == "TYPE OF FACILITY" ~ "faci_type",
          row2 == "OWNERSHIP" ~ "ownership",
+         row2 == "OWNERHSIP" ~ "ownership",
          row2 == "MUN/CITY" ~ "muncity",
+         row2 == "MUNCITY" ~ "muncity",
          row2 == "PROVINCE" ~ "province",
          row2 == "REMARKS" ~ "remarks",
+         grepl("^1- PRIVATE HOSPITAL", row2) ~ "faci_type",
          TRUE ~ row2
       )
-      log_warn("row2")
-      print(unique(row2))
 
       row3 <- case_when(
          grepl("^OFFICIAL NAME", row3) ~ NA_character_,
@@ -110,6 +133,8 @@ labbs_sheet <- function(xl, disease, sheet_name) {
          row3 == "1 - GOVERNMENT; 2 - PRIVATE" ~ "pubpriv",
          row3 == "MALE" ~ "m",
          row3 == "M" ~ "m",
+         row3 == "F" ~ "f",
+         row3 == "FEMALE" ~ "f",
          row3 == "FEMALE (TOTAL FEMALES)" ~ "f",
          row3 == "FEMALE &#10;(TOTAL FEMALES)" ~ "f",
          row3 == "TOTAL F" ~ "f",
@@ -117,14 +142,33 @@ labbs_sheet <- function(xl, disease, sheet_name) {
          row3 == "PREGNANT" ~ "preg",
          row3 == "TOTALB" ~ "total",
          row3 == "TOTAL" ~ "total",
+         is.na(row3) & form_version_1 == "*Please indicate the corresponding number" ~ "total",
+         is.na(row3) & form_version_2 == "*Please indicate the corresponding number" ~ "total",
+         row3 == "" & form_version_1 == "*Please indicate the corresponding number" ~ "total",
+         row3 == "" & form_version_2 == "*Please indicate the corresponding number" ~ "total",
+         row3 == "NA" & form_version_1 == "*Please indicate the corresponding number" ~ "total",
+         row3 == "NA" & form_version_2 == "*Please indicate the corresponding number" ~ "total",
          TRUE ~ row3
       )
+      # check weird unmerging of old forms
+      for (i in seq_len(length(row3)))
+         row3[i] <- ifelse(is.na(row3[i]) & grepl("^num", row2[i]), "total", row3[i])
 
       # concatenate all rows into one name each, then assign to the dataset
       final_names <- paste(row1, row2, row3, sep = "_")
       final_names <- case_when(
          final_names == "1_2_3" ~ "last_col",
+         final_names == "3_4_5" ~ "last_col",
+         final_names == "4_5_6" ~ "last_col",
+         final_names == "5_6_7" ~ "last_col",
          final_names == "5_6_8" ~ "last_col",
+         final_names == "6_7_9" ~ "last_col",
+         final_names == "7_8_9" ~ "last_col",
+         final_names == "7_8_10" ~ "last_col",
+         final_names == "5_6_NA" ~ "last_col",
+         final_names == "547_548_549" ~ "last_col",
+         final_names == "muncity_muncity_NA" ~ "muncity",
+         final_names == "muncity_muncity_muncity" ~ "muncity",
          grepl("^faci_name", final_names) ~ "faci_name",
          grepl("^faci_type", final_names) ~ "faci_type",
          grepl("^ownership", final_names) ~ "pubpriv",
@@ -133,7 +177,7 @@ labbs_sheet <- function(xl, disease, sheet_name) {
          grepl("^remarks", final_names) ~ "faci_status",
          TRUE ~ final_names
       )
-      names(data) <- final_names
+      names(data) <- gsub("_2$", "", final_names)
 
       # final cleaning of dataset and conversion to standard format
       data %<>%
@@ -148,7 +192,7 @@ labbs_sheet <- function(xl, disease, sheet_name) {
       if (col_faci_name == 1)
          data %<>%
             mutate(
-               reg_faci_id = row_number(),
+               reg_faci_id = as.character(row_number()),
                .before     = 1
             )
 
@@ -162,8 +206,105 @@ labbs_sheet <- function(xl, disease, sheet_name) {
          filter(!is.na(reg_faci_id), faci_name != "0") %>%
          mutate(region = region) %>%
          relocate(any_of(c("region", "province", "muncity")), .after = reg_faci_id) %>%
-         remove_empty("cols")
+         mutate(labbs_disease = disease) %>%
+         select(
+            -starts_with("q1"),
+            -starts_with("q2"),
+            -starts_with("q3"),
+            -starts_with("q4"),
+            -starts_with("x2018"),
+            -starts_with("jan_dec"),
+            -starts_with("na_na_na"),
+            -starts_with("annual"),
+            -starts_with("testing_"),
+            -starts_with("reports_"),
+         )
 
+      underscore2 <- get_names(data, "_2$")
+      if (length(underscore2))
+         print(underscore2)
+
+      underscoreNA <- get_names(data, "_na$")
+      if (length(underscoreNA))
+         print(underscoreNA)
+
+      underscoreNUM <- get_names(data, "_[0-9]+$")
+      if (length(underscoreNUM))
+         print(underscoreNUM)
+
+      underscoreS <- get_names(data, "_s$")
+      if (length(underscoreS))
+         print(underscoreS)
+
+      non_standard_cols <- data %>%
+         slice(1) %>%
+         select(
+            -any_of(c(
+               "reg_faci_id",
+               "faci_name",
+               "region",
+               "province",
+               "muncity",
+               "labbs_disease",
+               "pubpriv",
+               "faci_status",
+               "var",
+               "month",
+               "value"
+            ))
+
+               -
+               starts_with("x01_"),
+            -starts_with("x02_"),
+            -starts_with("x03_"),
+            -starts_with("x04_"),
+            -starts_with("x05_"),
+            -starts_with("x06_"),
+            -starts_with("x07_"),
+            -starts_with("x08_"),
+            -starts_with("x09_"),
+            -starts_with("x10_"),
+            -starts_with("x11_"),
+            -starts_with("x12_"),
+         ) %>%
+         get_names()
+      if (length(non_standard_cols))
+         print(non_standard_cols)
    }
    return(data)
+}
+
+
+labbs_agg <- function(data, id_cols, config) {
+
+   cols   <- config$cols %>%
+      filter(sheet == data[1,]$labbs_disease)
+   cols_1 <- paste0(toupper(cols$r_col), "_F")
+   cols_2 <- paste0(toupper(cols$r_col), "_M")
+   cols_3 <- paste0(toupper(cols$r_col), "_PREG")
+   cols_4 <- paste0(toupper(cols$r_col), "_TOTAL")
+
+   group_vars <- c(id_cols, "var")
+   aggregate  <- data %>%
+      group_by(across(all_of(group_vars))) %>%
+      summarise(value = sum(value, na.rm = TRUE)) %>%
+      ungroup() %>%
+      pivot_wider(
+         id_cols     = {{id_cols}},
+         names_from  = var,
+         values_from = value
+      ) %>%
+      select(
+      {{id_cols}},
+      any_of(cols_1),
+      any_of(cols_2),
+      any_of(cols_3),
+      any_of(cols_4),
+      ) %>%
+      mutate_if(
+         .predicate = is.numeric,
+         ~as.integer(coalesce(., 0))
+      )
+
+   return(aggregate)
 }
--- src/official/labbs/00_main.R
+++ src/official/labbs/00_main.R
@@ -1,5 +1,7 @@
 labbs <- new.env()
 
+##  Set reporting parameters to be used for the project ------------------------
+
 # set paths and reporting period
 local(envir = labbs, {
    params        <- list()
@@ -11,7 +13,8 @@ local(envir = labbs, {
    params$ym     <- paste0(params$yr, ".", params$mo)
 })
 
-# get gdrive configs
+##  Set configurations and parameters for the files and reports ----------------
+
 local(envir = labbs, {
    # get submissions
    params$gdrive_ym <- drive_ls(params$gdrive, params$ym)$id
@@ -25,37 +28,160 @@ local(envir = labbs, {
    params$gdrive_submit <- filter(paths, name == "Submissions")$id
    params$gdrive_valid  <- filter(paths, name == "Validations")$id
    rm(paths)
+
+   dirs             <- list()
+   dirs$local_files <- file.path(Sys.getenv("LABBS"), params$ym)
+
+   reports <- list(
+      faci_list = "List of Facilities",
+      hiv       = "HIV",
+      bbhiv     = "BLOODHIV",
+      hepb      = "Hepatitis B",
+      bbhepb    = "BLOODHepB",
+      hepc      = "Hepatitis C",
+      bbhepc    = "BLOODHepC",
+      syp       = "Syphilis",
+      bbsyp     = "BLOODSyph",
+      gono      = "Gonorrhea"
+   )
+
+   files <- drive_ls(labbs$params$gdrive_submit) %>%
+      mutate(local = file.path(dirs$local_files, name))
 })
 
-# 1) get list of submissions from LaBBS-DQT drive for the quarter
-# 2) download files into local directory
-files_submit <- drive_ls(labbs$params$gdrive_submit)
-invisible(lapply(seq_len(nrow(files_submit)), function(i, drive_files, params) {
-   file   <- drive_files[i,]$id
-   output <- file.path(Sys.getenv("LABBS"), params$ym, drive_files[i,]$name)
-   drive_download(file, output, overwrite = TRUE)
-}, files_submit, labbs$params))
+##  Get and process files for the reporting period -----------------------------
 
+# download files into local directory
+local(envir = labbs, {
+   log_warn("Downloading submissions from GDrive.")
+   invisible(apply(files, 1, function(row) {
+      log_info("Downloading {green(row$name)}.")
+      local_drive_quiet()
+      drive_download(as_id(row$id), row$local, overwrite = TRUE)
+   }))
+   log_success("Done downloading!")
+})
 
-# 1) get list of downloaded excel files
-files_submit <- dir_ls(file.path(Sys.getenv("LABBS"), labbs$params$ym))
+# import excel data and process per sheet
+labbs$data        <- lapply(seq_along(labbs$reports), function(i, env) {
+   # import each region as lists of all region with submissions on the specific
+   # diseases. sheet_data is initially a list object
+   log_warn("Sheet: {red(env$reports[[i]])}")
+   sheet_data <- lapply(env$files$local, labbs_sheet, names(env$reports)[[i]], env$reports[[i]], env)
 
-labbs_reports     <- list(
-   faci_list = "List of Facilities",
-   hiv       = "HIV",
-   bbhiv     = "BLOODHIV",
-   hepb      = "Hepatitis B",
-   bbhepb    = "BLOODHepB",
-   hepc      = "Hepatitis C",
-   bbhepc    = "BLOODHepC",
-   syp       = "Syphilis",
-   bbsyp     = "BLOODSyph",
-   gono      = "Gonorrhea"
-)
-labbs$data        <- lapply(seq_len(length(labbs_reports)), function(i, files_submit) {
-   sheet_data <- lapply(files_submit, labbs_sheet, names(labbs_reports)[[i]], labbs_reports[[i]])
+   # bind all datasets into a single dataframe, each sheet will have all data
+   # for that specific disease across all regions
    sheet_data <- bind_rows(sheet_data)
+
+   log_success("Done!")
    return(sheet_data)
-}, files_submit)
-names(labbs$data) <- names(labbs_reports)
-invisible(lapply(files_submit, labbs_sheet, "gono", "Gonorrhea"))
+}, labbs)
+names(labbs$data) <- names(labbs$reports)
+
+# clean facilities reference list
+labbs$data$faci_list %<>%
+   distinct(region, province, muncity, faci_name, faci_type, faci_status, pubpriv, .keep_all = TRUE) %>%
+   mutate_if(
+      .predicate = is.character,
+      ~toupper(str_squish(.))
+   )
+
+# convert the bound datasets into long format;
+# each row will be a combination of indicator, per month, per disease, per facility;
+# facilities list sheet (first sheet) will be excluded from conversion
+labbs$long <- lapply(labbs$data[2:10], function(data) {
+   long <- data %>%
+      pivot_longer(
+         cols     = starts_with("x"),
+         names_to = "month_var"
+      ) %>%
+      mutate(
+         # extract variable name from grouping variable
+         var   = substr(month_var, 5, 1000),
+
+         # extract month from grouping variable
+         month = substr(month_var, 2, 3),
+
+         # tag data to be dropped
+         drop  = case_when(
+            faci_name == "#ERROR!" ~ 1,
+            faci_name == "" ~ 1,
+            TRUE ~ 0
+         )
+      ) %>%
+      filter(drop == 0) %>%
+      mutate_at(
+         .vars = vars(faci_name, region, province, var),
+         ~toupper(str_squish(.))
+      ) %>%
+      # only keep columns that are relevant for analysis
+      select(
+         any_of(c(
+            "reg_faci_id",
+            "faci_name",
+            "region",
+            "province",
+            "muncity",
+            "labbs_disease",
+            "pubpriv",
+            "faci_status",
+            "var",
+            "month",
+            "value"
+         ))
+      ) %>%
+      # keep only rows that start with num; these are the indicator columns
+      filter(grepl("^NUM", var)) %>%
+      mutate(
+         # tag per quarter
+         qr    = case_when(
+            month %in% c("01", "02", "03") ~ "Q1",
+            month %in% c("04", "05", "06") ~ "Q2",
+            month %in% c("07", "08", "09") ~ "Q3",
+            month %in% c("10", "11", "12") ~ "Q4",
+         ),
+
+         # initial cleaning for non-numeric data that were accidentally included
+         # in the final consolidation sheets
+         value = str_squish(value),
+         value = gsub("\\.0$", "", value),
+         value = case_when(
+            value == "c" ~ NA_character_,
+            value == "TRUE" ~ NA_character_,
+            value == "FALSE" ~ NA_character_,
+            value == ":" ~ NA_character_,
+            grepl("^[a-zA-Z]+$", value) ~ NA_character_,
+            TRUE ~ value
+         ),
+         value = as.numeric(value)
+      )
+})
+
+labbs$agg    <- list()
+labbs$agg$qr <- lapply(labbs$long, labbs_agg, c("faci_name", "region", "province", "qr"), labbs$config)
+labbs$agg$yr <- lapply(labbs$long, labbs_agg, c("faci_name", "region", "province"), labbs$config)
+
+labbs_review <- bind_rows(labbs$long) %>%
+   distinct(region, province, faci_name)
+
+labbs$data$faci_list %>%
+   select(region, province, muncity, faci_name, faci_type, faci_status, pubpriv) %>%
+   write_sheet("1TNxJrnV_yjvvKW3Pb9eNuFfJphyl6CPyxuAOQPDo8sc", "for_review_tebers")
+
+ohasis$ref_faci %>%
+   select(
+      FACI_ID,
+      SUB_FACI_ID,
+      FACI_NAME,
+      EMAIL,
+      MOBILE,
+      LANDLINE,
+      FACI_PSGC_REG,
+      FACI_PSGC_PROV,
+      FACI_PSGC_MUNC,
+      FACI_NAME_REG,
+      FACI_NAME_PROV,
+      FACI_NAME_MUNC,
+      FACI_ADDR
+   ) %>%
+   write_sheet("1TNxJrnV_yjvvKW3Pb9eNuFfJphyl6CPyxuAOQPDo8sc", "ref_faci")
