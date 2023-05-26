94b5c95c5a31a23a8d4f8e96980e390f202016d5
jrpalo.doh <jrpalo@doh.gov.ph>
2023-05-26 22:17:11 +0800

    initial code/comments for tly data integration

---
+++ src/misc/tly_siu/00_main.R
@@ -0,0 +1,36 @@
+##  HTS ------------------------------------------------------------------------
+
+# 1) Testing Decking Sheet
+
+
+##  ARVs -----------------------------------------------------------------------
+
+# 1) New Chapter Database
+# 2) ARV Refill Date Tracker
+
+##  PrEP -----------------------------------------------------------------------
+
+# 1) PrEPPY Masterfile
+
+##  SelfCare -------------------------------------------------------------------
+
+# 1) Googlesheet directly from chatbot
+#    - import directly from three sheets
+
+##  VL Test --------------------------------------------------------------------
+
+# 1) VL Confidential Masterlist
+
+##  TB Screening ---------------------------------------------------------------
+
+# 1) Consultation Monitoring Sheet (June 2023)
+
+##  Labs -----------------------------------------------------------------------
+
+# 1) Internal Lab Sheet (Laboratory Database 2023)
+
+##  TPT ------------------------------------------------------------------------
+
+# 1) Pull live from TPT Masterlist
+
+##  Frequency (Weekly on Wednesdays, 6PM)
