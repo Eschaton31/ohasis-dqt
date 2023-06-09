f126ac1c39a1d6ca5ce31ae0890bf228e38183da
Gitea <gitea@fake.local>
2023-06-09 17:16:29 +0800

    patch form_a to remove those without forms yet

--- src/official/harp_dx/form_a.sql
+++ src/official/harp_dx/form_a.sql
@@ -3,6 +3,7 @@ SELECT IF(id_registry.CENTRAL_ID IS NULL, hts_data.PATIENT_ID, id_registry.CENTR
 FROM ohasis_warehouse.form_a AS hts_data
          LEFT JOIN ohasis_warehouse.id_registry ON hts_data.PATIENT_ID = id_registry.PATIENT_ID
 WHERE LEFT(hts_data.CONFIRM_RESULT, 1) = '1'
+  AND hts_data.FORM_VERSION IS NOT NULL
   AND hts_data.REC_ID NOT IN (SELECT REC_ID FROM ohasis_warehouse.harp_dx_old)
   AND IF(id_registry.CENTRAL_ID IS NULL, hts_data.PATIENT_ID, id_registry.CENTRAL_ID) NOT IN
       (SELECT CENTRAL_ID FROM ohasis_warehouse.harp_dx_old);
