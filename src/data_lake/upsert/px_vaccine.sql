930c74b86ec512bb6d95ee450f94238c50ef408a
jrpalo.doh <jrpalo@doh.gov.ph>
2023-06-27 22:27:32 +0800

    converted from left join to inner join

--- src/data_lake/upsert/px_vaccine.sql
+++ src/data_lake/upsert/px_vaccine.sql
@@ -37,22 +37,22 @@ SELECT rec.REC_ID,
        MAX(IF(vax.DISEASE_VAX = 'HEPB' AND vax.VAX_NUM = 3, vax.VAX_REMARKS,
               NULL))                                                                                   AS VAX_HEPB_3RD_LOCATION
 FROM ohasis_interim.px_record AS rec
-         LEFT JOIN (SELECT REC_ID,
-                           CASE DISEASE_VAX
-                               WHEN '102000' THEN 'HEPB'
-                               WHEN '201000' THEN 'COVID19'
-                               END           AS DISEASE_VAX,
-                           CASE IS_VAX
-                               WHEN 1 THEN '1_Yes'
-                               WHEN 2 THEN '2_Refused'
-                               WHEN 0 THEN '0_No'
-                               ELSE NULL END AS IS_VAX,
-                           VAX_NUM,
-                           VAX_USED,
-                           VAX_DATE,
-                           VAX_REMARKS,
-                           VAX_RESULT
-                    FROM ohasis_interim.px_vaccine) AS vax ON rec.REC_ID = vax.REC_ID
+         JOIN (SELECT REC_ID,
+                      CASE DISEASE_VAX
+                          WHEN '102000' THEN 'HEPB'
+                          WHEN '201000' THEN 'COVID19'
+                          END           AS DISEASE_VAX,
+                      CASE IS_VAX
+                          WHEN 1 THEN '1_Yes'
+                          WHEN 2 THEN '2_Refused'
+                          WHEN 0 THEN '0_No'
+                          ELSE NULL END AS IS_VAX,
+                      VAX_NUM,
+                      VAX_USED,
+                      VAX_DATE,
+                      VAX_REMARKS,
+                      VAX_RESULT
+               FROM ohasis_interim.px_vaccine) AS vax ON rec.REC_ID = vax.REC_ID
 WHERE ((rec.CREATED_AT BETWEEN ? AND ?) OR
        (rec.UPDATED_AT BETWEEN ? AND ?) OR
        (rec.DELETED_AT BETWEEN ? AND ?))
