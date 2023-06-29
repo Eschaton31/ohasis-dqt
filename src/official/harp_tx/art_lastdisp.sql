ae630e1ccd6db4b1d610a894fc39b3f1d0615cf3
jrpalo.doh <jrpalo@doh.gov.ph>
2023-06-29 21:46:14 +0800

    formatting fix

--- src/official/harp_tx/art_lastdisp.sql
+++ src/official/harp_tx/art_lastdisp.sql
@@ -4,13 +4,10 @@ FROM (SELECT data.CENTRAL_ID,
              data.VISIT_DATE,
              ROW_NUMBER() OVER (PARTITION BY CENTRAL_ID ORDER BY VISIT_DATE DESC) AS VISIT_NUM
       FROM (
-               SELECT CASE
-                          WHEN reg.CENTRAL_ID IS NULL THEN rec.PATIENT_ID
-                          WHEN reg.CENTRAL_ID IS NOT NULL THEN reg.CENTRAL_ID
-                          END AS CENTRAL_ID,
+               SELECT COALESCE(reg.CENTRAL_ID, rec.PATIENT_ID) AS CENTRAL_ID,
                       rec.*
-               FROM ohasis_warehouse.form_art_bc rec
-                        LEFT JOIN ohasis_warehouse.id_registry reg ON rec.PATIENT_ID = reg.PATIENT_ID
+               FROM ohasis_warehouse.form_art_bc AS rec
+                        LEFT JOIN ohasis_warehouse.id_registry AS reg ON rec.PATIENT_ID = reg.PATIENT_ID
                WHERE MEDICINE_SUMMARY IS NOT NULl AND VISIT_DATE < ?
            ) AS data) AS artstart
 WHERE VISIT_NUM = 1;
