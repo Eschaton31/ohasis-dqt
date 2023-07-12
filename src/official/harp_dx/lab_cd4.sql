4cb3dc6f053afcfb60d690a9c23740cc61c1087e
John Benedict Palo <jrpalo@doh.gov.ph>
2023-07-12 13:07:10 +0800

    utilized the new max params

--- src/official/harp_dx/lab_cd4.sql
+++ src/official/harp_dx/lab_cd4.sql
@@ -3,4 +3,4 @@ SELECT IF(id_registry.CENTRAL_ID IS NULL, cd4_data.PATIENT_ID, id_registry.CENTR
        cd4_data.CD4_RESULT
 FROM ohasis_lake.lab_cd4 AS cd4_data
          LEFT JOIN ohasis_warehouse.id_registry ON cd4_data.PATIENT_ID = id_registry.PATIENT_ID
-WHERE cd4_data.DELETED_AT IS NULL AND CD4_DATE < ?;
+WHERE cd4_data.DELETED_AT IS NULL AND DATE(CD4_DATE) <= ?;
--- src/official/harp_tx/vl_last.sql
+++ src/official/harp_tx/vl_last.sql
@@ -11,5 +11,5 @@ FROM (SELECT data.CENTRAL_ID,
                      LEFT JOIN ohasis_lake.px_pii AS pii ON rec.REC_ID = pii.REC_ID
             WHERE rec.DELETED_AT IS NULL
               AND rec.LAB_VIRAL_RESULT IS NOT NULL
-              AND COALESCE(rec.LAB_VIRAL_DATE, pii.RECORD_DATE) < ?) AS data) AS artstart
+              AND DATE(COALESCE(rec.LAB_VIRAL_DATE, pii.RECORD_DATE)) <= ?) AS data) AS artstart
 WHERE VISIT_NUM = 1;
