425fb5bebb6c8882b45294be1e688e171a09a7b5
John Benedict Palo <jrpalo@doh.gov.ph>
2023-07-13 14:02:29 +0800

    added ref_faci to conversion of dxlab_standard

--- src/official/dsa/pepfar/00_main.R
+++ src/official/dsa/pepfar/00_main.R
@@ -1,5 +1,5 @@
 flow_register()
-pepfar$steps$`01_load_reqs`$.init(yr = 2023, mo = 3, report = "HFR", envir = pepfar)
+pepfar$steps$`01_load_reqs`$.init(yr = 2023, mo = 6, report = "HFR", envir = pepfar)
 pepfar$steps$`02_prepare_tx`$.init(pepfar)
 pepfar$steps$`03_prepare_prep`$.init(pepfar)
 pepfar$steps$`04_prepare_reach`$.init(pepfar)
@@ -58,3 +58,185 @@ pepfar$ip$EpiC$linelist$reach %>%
    write_xlsx(file.path(oh_dir, glue("Linelist-HTS-PREV.xlsx")))
 
 jsonlite::write_json(epic$data$json, file_json, pretty = TRUE, auto_unbox = TRUE)
+
+
+tx <- hs_data("harp_tx", "reg", 2023, 4) %>%
+   read_dta(col_select = c(art_id, PATIENT_ID)) %>%
+   left_join(
+      y  = hs_data("harp_tx", "outcome", 2023, 4) %>%
+         read_dta(),
+      by = join_by(art_id)
+   ) %>%
+   get_cid(pepfar$forms$id_reg, PATIENT_ID) %>%
+   faci_code_to_id(
+      ohasis$ref_faci_code,
+      c(TX_FACI = "hub", TX_SUB_FACI = "branch")
+   ) %>%
+   faci_code_to_id(
+      ohasis$ref_faci_code,
+      c(REAL_FACI = "realhub", REAL_SUB_FACI = "realhub_branch")
+   ) %>%
+   mutate_at(
+      .vars = vars(TX_FACI, REAL_FACI),
+      ~if_else(. == "130000", NA_character_, ., .),
+   ) %>%
+   distinct(art_id, .keep_all = TRUE) %>%
+   inner_join(
+      y  = pepfar$coverage$sites %>%
+         filter(site_epic_2022 == 1) %>%
+         select(REAL_FACI = FACI_ID),
+      by = join_by(REAL_FACI)
+   ) %>%
+   remove_pii() %>%
+   select(-starts_with("tx")) %>%
+   ohasis$get_faci(
+      list(tx_hub = c("REAL_FACI", "REAL_SUB_FACI")),
+      "name",
+      c("tx_reg", "tx_prov", "tx_munc")
+   ) %>%
+   select(
+      REC_ID,
+      CENTRAL_ID,
+      art_id,
+      idnum,
+      prep_id,
+      mort_id,
+      sex,
+      vl_date,
+      vl_result,
+      baseline_vl,
+      vl_suppressed,
+      vlp12m,
+      curr_age,
+      artstart_date,
+      class,
+      tx_hub,
+      tx_reg,
+      tx_prov,
+      tx_munc,
+      outcome,
+      onart,
+      latest_ffupdate,
+      latest_nextpickup,
+      latest_regimen,
+      previous_ffupdate,
+      previous_nextpickup,
+      previous_regimen,
+      who_staging,
+      tb_status,
+      oi_syph,
+      oi_hepb,
+      oi_hepc,
+      oi_pcp,
+      oi_cmv,
+      oi_orocand,
+      oi_herpes,
+      oi_other,
+   )
+
+prep <- hs_data("prep", "reg", 2023, 4) %>%
+   read_dta(col_select = c(prep_id, PATIENT_ID)) %>%
+   left_join(
+      y  = hs_data("prep", "outcome", 2023, 4) %>%
+         read_dta() %>%
+         select(-PATIENT_ID),
+      by = join_by(prep_id)
+   ) %>%
+   get_cid(pepfar$forms$id_reg, PATIENT_ID) %>%
+   faci_code_to_id(
+      ohasis$ref_faci_code,
+      c(PREP_FACI = "faci", PREP_SUB_FACI = "branch")
+   ) %>%
+   mutate_at(
+      .vars = vars(PREP_FACI),
+      ~if_else(. == "130000", NA_character_, ., .),
+   ) %>%
+   distinct(prep_id, .keep_all = TRUE) %>%
+   inner_join(
+      y  = pepfar$coverage$sites %>%
+         filter(site_epic_2022 == 1) %>%
+         select(PREP_FACI = FACI_ID),
+      by = join_by(PREP_FACI)
+   ) %>%
+   remove_pii() %>%
+   select(-prep_reg, -prep_prov, -prep_munc) %>%
+   ohasis$get_faci(
+      list(prep_hub = c("PREP_FACI", "PREP_SUB_FACI")),
+      "name",
+      c("prep_reg", "prep_prov", "prep_munc")
+   ) %>%
+   select(
+      REC_ID,
+      CENTRAL_ID,
+      prep_id,
+      idnum,
+      art_id,
+      mort_id,
+      sex,
+      self_identity,
+      self_identity_other,
+      gender_identity,
+      with_hts,
+      HTS_REC,
+      hts_form,
+      hts_modality,
+      hts_result,
+      hts_date,
+      prep_hts_date,
+      prep_risk_sexwithf,
+      prep_risk_sexwithf_nocdm,
+      prep_risk_sexwithm,
+      prep_risk_sexwithm_nocdm,
+      prep_risk_injectdrug,
+      prep_risk_chemsex,
+      prep_risk_sextransaction,
+      prep_risk_sexwithplhiv_novl,
+      prep_risk_sexunknownhiv,
+      prep_risk_avgsexweek,
+      hts_risk_motherhashiv,
+      hts_risk_sexwithf,
+      hts_risk_sexwithf_nocdm,
+      hts_risk_sexwithm,
+      hts_risk_sexwithm_nocdm,
+      hts_risk_payingforsex,
+      hts_risk_paymentforsex,
+      hts_risk_sexwithhiv,
+      hts_risk_injectdrug,
+      hts_risk_needlestick,
+      hts_risk_bloodtransfuse,
+      hts_risk_illicitdrug,
+      hts_risk_chemsex,
+      hts_risk_tattoo,
+      hts_risk_sti,
+      kp_pdl,
+      kp_tg,
+      kp_pwid,
+      kp_msm,
+      kp_sw,
+      kp_other,
+      prepstart_date,
+      prep_reinit_date,
+      prep_first_time,
+      prep_plan,
+      prep_type,
+      prep_status,
+      curr_age,
+      prep_hub,
+      prep_reg,
+      prep_prov,
+      prep_munc,
+      outcome,
+      onprep,
+      latest_ffupdate,
+      latest_nextpickup,
+      latest_regimen,
+   )
+
+dir <- "O:/My Drive/Data Sharing/EpiC/For Kuya Chard - 20230614"
+tx %>%
+   format_stata() %>%
+   write_dta(file.path(dir, "20230614_onart-vl_EpiC_2023-04.dta"))
+
+prep %>%
+   format_stata() %>%
+   write_dta(file.path(dir, "20230614_onprep_EpiC_2023-04.dta"))
