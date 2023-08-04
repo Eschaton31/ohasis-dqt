31511ea8944afcd9b29620379ba33b3fe3cdf64f
John Benedict Palo <jrpalo@doh.gov.ph>
2023-08-05 03:48:06 +0800

    improved tagging of tbhiv indicators, added itis data

--- src/official/tbhiv/02_process_visits.R
+++ src/official/tbhiv/02_process_visits.R
@@ -1,9 +1,142 @@
 ##  Filter Initial Data & Remove Already Reported ------------------------------
 
-standardize_visits <- function(form_art, coverage) {
+standardize_visits <- function(forms, coverage, itis_tpt = NULL, itis_drtb = NULL) {
+   if (!is.null(itis_tpt)) {
+      log_info("Processing ITIS TPT.")
+
+      itis_tpt %<>%
+         inner_join(forms$id_itis, join_by(ITIS_PATIENT_ID)) %>%
+         get_cid(forms$id_reg, PATIENT_ID) %>%
+         mutate(
+            LAB_XPERT_RESULT = str_c(sep = "; ", XPERT_RESULT_MTB, XPERT_RESULT_RIF),
+            LAB_XRAY_RESULT  = str_c(sep = "; ", X_RAY_PRESENTATION, X_RAY_EVOLUTION),
+            TB_IPT_STATUS    = case_when(
+               OUTCOME_STATUS == "ON TREATMENT" ~ "11_Ongoing IPT",
+               OUTCOME_STATUS == "TREATMENT COMPLETED" ~ "13_Ended IPT",
+               OUTCOME_STATUS == "CURED" ~ "13_Ended IPT",
+               OUTCOME_STATUS == "COMPLETED" ~ "13_Ended IPT",
+               OUTCOME_STATUS == "NOT EVALUATED" ~ "13_Ended IPT",
+               OUTCOME_STATUS == "FAILED" ~ "13_Ended IPT",
+               OUTCOME_STATUS == "DIAGNOSED" ~ "13_Ended IPT",
+               OUTCOME_STATUS == "LOST TO FF-UP" ~ "13_Ended IPT",
+            ),
+            TB_IPT_STATUS    = if_else(is.na(DATE_STARTED_TX), NA_character_, TB_IPT_STATUS, TB_IPT_STATUS),
+            TB_IPT_OUTCOME   = case_when(
+               OUTCOME_STATUS == "ON TREATMENT" ~ "8888_Other",
+               OUTCOME_STATUS == "TREATMENT COMPLETED" ~ "1_Completed",
+               OUTCOME_STATUS == "CURED" ~ "1_Completed",
+               OUTCOME_STATUS == "COMPLETED" ~ "1_Completed",
+               OUTCOME_STATUS == "NOT EVALUATED" ~ "8888_Other",
+               OUTCOME_STATUS == "FAILED" ~ "1_Completed",
+               OUTCOME_STATUS == "LOST TO FF-UP" ~ "2_Stopped before target end",
+            ),
+            TB_IPT_OUTCOME   = if_else(is.na(DATE_STARTED_TX), NA_character_, TB_IPT_OUTCOME, TB_IPT_OUTCOME)
+         ) %>%
+         mutate_at(
+            .vars = vars(ends_with("RESULT")),
+            ~na_if(., "; ") %>%
+               na_if("Not Done; N/A")
+         ) %>%
+         mutate_if(
+            .predicate = is.Date,
+            ~na_if(., as.Date("1970-01-01"))
+         ) %>%
+         mutate_if(
+            .predicate = is.character,
+            ~na_if(str_squish(.), "")
+         ) %>%
+         select(
+            CENTRAL_ID,
+            PATIENT_ID,
+            VISIT_DATE        = REGISTRATION_DATE,
+            LAB_XPERT_DATE    = XPERT_SPECIMEN_RELEASE_DATE,
+            LAB_XPERT_RESULT,
+            LAB_XRAY_DATE     = X_RAY_DATE,
+            LAB_XRAY_RESULT,
+            LAB_DSSM_DATE     = DSSM_RELEASE_DATE,
+            LAB_DSSM_RESULT   = DSSM_RESULT,
+            TB_IPT_START_DATE = DATE_STARTED_TX,
+            TB_IPT_END_DATE   = TREATMENT_END_DATE,
+            TB_IPT_STATUS,
+            TB_IPT_OUTCOME
+         ) %>%
+         mutate(
+            TB_SCREEN = "1_Yes",
+            TB_STATUS = "0_No active TB"
+         )
+   }
+   if (!is.null(itis_drtb)) {
+      log_info("Processing ITIS DRTB.")
+
+      itis_drtb %<>%
+         inner_join(forms$id_itis, join_by(ITIS_PATIENT_ID)) %>%
+         get_cid(forms$id_reg, PATIENT_ID) %>%
+         mutate(
+            LAB_XPERT_RESULT = str_c(sep = "; ", XPERT_RESULT_MTB, XPERT_RESULT_RIF),
+            LAB_XRAY_RESULT  = str_c(sep = "; ", X_RAY_PRESENTATION, X_RAY_EVOLUTION),
+            TB_TX_STATUS     = case_when(
+               OUTCOME_STATUS == "ON TREATMENT" ~ "11_Ongoing Tx",
+               OUTCOME_STATUS == "TREATMENT COMPLETED" ~ "13_Ended Tx",
+               OUTCOME_STATUS == "CURED" ~ "13_Ended Tx",
+               OUTCOME_STATUS == "COMPLETED" ~ "13_Ended Tx",
+               OUTCOME_STATUS == "NOT EVALUATED" ~ "13_Ended Tx",
+               OUTCOME_STATUS == "DIED" ~ "13_Ended Tx",
+               OUTCOME_STATUS == "FAILED" ~ "13_Ended Tx",
+               OUTCOME_STATUS == "DIAGNOSED" ~ "13_Ended Tx",
+               OUTCOME_STATUS == "LOST TO FF-UP" ~ "13_Ended Tx",
+            ),
+            TB_TX_STATUS     = if_else(is.na(DATE_STARTED_TX), NA_character_, TB_TX_STATUS, TB_TX_STATUS),
+            TB_TX_OUTCOME    = case_when(
+               OUTCOME_STATUS == "ON TREATMENT" ~ "8888_Other",
+               OUTCOME_STATUS == "TREATMENT COMPLETED" ~ "10_Not yet evaluated",
+               OUTCOME_STATUS == "CURED" ~ "11_Cured",
+               OUTCOME_STATUS == "NOT EVALUATED" ~ "10_Not yet evaluated",
+               OUTCOME_STATUS == "DIED" ~ "20_Failed",
+               OUTCOME_STATUS == "FAILED" ~ "20_Failed",
+               OUTCOME_STATUS == "LOST TO FF-UP" ~ "8888_Other",
+            ),
+            TB_TX_OUTCOME    = if_else(is.na(DATE_STARTED_TX), NA_character_, TB_TX_OUTCOME, TB_TX_OUTCOME)
+         ) %>%
+         select(
+            CENTRAL_ID,
+            PATIENT_ID,
+            VISIT_DATE       = REGISTRATION_DATE,
+            LAB_XPERT_DATE   = XPERT_SPECIMEN_RELEASE_DATE,
+            LAB_XPERT_RESULT,
+            LAB_XRAY_DATE    = X_RAY_DATE,
+            LAB_XRAY_RESULT,
+            LAB_DSSM_DATE    = DSSM_RELEASE_DATE,
+            LAB_DSSM_RESULT  = DSSM_RESULT,
+            TB_TX_START_DATE = DATE_STARTED_TX,
+            TB_TX_END_DATE   = TREATMENT_END_DATE,
+            TB_REGIMEN       = TREATMENT_REGIMEN,
+            TB_TX_STATUS,
+            TB_TX_OUTCOME
+         ) %>%
+         mutate_at(
+            .vars = vars(ends_with("RESULT")),
+            ~na_if(., "; ") %>%
+               na_if("Not Done; N/A")
+         ) %>%
+         mutate_if(
+            .predicate = is.Date,
+            ~na_if(., as.Date("1970-01-01"))
+         ) %>%
+         mutate_if(
+            .predicate = is.character,
+            ~na_if(str_squish(.), "")
+         ) %>%
+         mutate(
+            TB_SCREEN = "1_Yes",
+            TB_STATUS = "1_With active TB"
+         )
+   }
+
    log_info("Standardizing visits.")
    curr_yr <- as.numeric(coverage$yr)
-   data    <- form_art %>%
+   data    <- forms$form_art_bc %>%
+      bind_rows(itis_tpt) %>%
+      bind_rows(itis_drtb) %>%
       # tag tbhiv info
       mutate(
          visit_yr         = year(VISIT_DATE),
@@ -110,31 +243,65 @@ group_coverage <- function(harp_tx, standard, min, max) {
       if (!is.infinite(x <- suppressWarnings(max(..., na.rm = na.rm)))) x else def
    }
 
-   data <- harp_tx %>%
+   tx_visits <- harp_tx %>%
       left_join(
          y        = standard %>%
             filter(VISIT_DATE %within% interval(min, max)),
          by       = join_by(CENTRAL_ID),
          multiple = "all"
       ) %>%
-      group_by(art_id) %>%
-      summarise(
-         visit         = .max(visit_cnt, na.rm = TRUE),
-         screened      = .max(visit_scrn, na.rm = TRUE),
-         screened_tri  = .max(visit_scrn_tri, na.rm = TRUE),
-         screened_tb   = .max(visit_scrn_tb, na.rm = TRUE),
-         screened_labs = .max(visit_scrn_labs, na.rm = TRUE),
-         withtb        = .max(visit_withtb, na.rm = TRUE),
-         startedipt    = .max(visit_startedipt, na.rm = TRUE),
-         onipt         = .max(visit_onipt, na.rm = TRUE),
-         endipt        = .max(visit_endipt, na.rm = TRUE),
-         startedtx     = .max(visit_startedtx, na.rm = TRUE),
-         ontx          = .max(visit_ontx, na.rm = TRUE),
-         endtx         = .max(visit_endtx, na.rm = TRUE),
-         curetx        = .max(visit_curetx, na.rm = TRUE),
-         failtx        = .max(visit_failtx, na.rm = TRUE),
+      select(
+         art_id,
+         visit         = visit_cnt,
+         screened      = visit_scrn,
+         screened_tri  = visit_scrn_tri,
+         screened_tb   = visit_scrn_tb,
+         screened_labs = visit_scrn_labs,
+         withtb        = visit_withtb,
+         startedipt    = visit_startedipt,
+         onipt         = visit_onipt,
+         endipt        = visit_endipt,
+         startedtx     = visit_startedtx,
+         ontx          = visit_ontx,
+         endtx         = visit_endtx,
+         curetx        = visit_curetx,
+         failtx        = visit_failtx,
+      )
+
+   flags <- c(
+      "visit",
+      "screened",
+      "screened_tri",
+      "screened_tb",
+      "screened_labs",
+      "withtb",
+      "startedipt",
+      "onipt",
+      "endipt",
+      "startedtx",
+      "ontx",
+      "endtx",
+      "curetx",
+      "failtx"
+   )
+   data  <- harp_tx %>% select(art_id)
+   for (flag in flags) {
+      flag <- as.name(flag)
+      data %<>%
+         left_join(
+            y  = tx_visits %>%
+               arrange(desc({{flag}})) %>%
+               select(art_id, {{flag}}) %>%
+               distinct(art_id, .keep_all = TRUE),
+            by = join_by(art_id)
+         )
+   }
+
+   data %<>%
+      mutate_at(
+         .vars = vars(all_of(flags)),
+         ~as.integer(coalesce(., 9999))
       ) %>%
-      ungroup() %>%
       left_join(
          y  = harp_tx %>%
             select(
@@ -211,8 +378,11 @@ group_coverage <- function(harp_tx, standard, min, max) {
 ##  Actual flow ----------------------------------------------------------------
 
 .init <- function(envir = parent.env(environment())) {
+
+   itis_tpt            <- read_dta("H:/_R/library/itis_tpt/20230701_itis-tpt_2023-06.dta")
+   itis_drtb           <- read_dta("H:/_R/library/itis_tbtx/20230701_itis-drtb_2023-06.dta")
    envir$data          <- list()
-   envir$data$standard <- standardize_visits(envir$forms$form_art_bc, envir$coverage)
+   envir$data$standard <- standardize_visits(envir$forms, envir$coverage, itis_tpt, itis_drtb)
 
    log_info("Creating linelist - {green('Monthly')}.")
    envir$data$mo <- group_coverage(envir$harp$tx$new, envir$data$standard, envir$coverage$min$mo, envir$coverage$max)
