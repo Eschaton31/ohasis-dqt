36a927bc771614843041fd604354e73a5ae04f48
John Benedict Palo <jrpalo@doh.gov.ph>
2023-09-04 09:05:51 +0800

    added function to pull latest pii data

--- src/pipeline/pii.R
+++ src/pipeline/pii.R
@@ -56,3 +56,66 @@ clean_pii <- function(pii_col) {
 
    return(clean)
 }
+
+get_latest_pii <- function(data, pid_col, pii_cols) {
+   for (col in pii_cols) {
+      missing <- data %>%
+         filter_at(.vars = vars(matches(col)), ~is.na(.))
+
+      if (nrow(missing) > 0) {
+         col_name <- as.name(col)
+         pids     <- stri_c(collapse = "','", missing[[pid_col]])
+         not_null <- ifelse(!str_detect(col, "PSGC"), col, stri_c(str_extract(col, "(.*_PSGC_).*", 1), "MUNC"))
+
+         lw_conn  <- ohasis$conn("lw")
+         data_cid <- dbTable(
+            lw_conn,
+            "ohasis_lake",
+            "px_pii",
+            cols      = c("PATIENT_ID", "SNAPSHOT", col),
+            join      = list(
+               "ohasis_warehouse.id_registry" = list(
+                  by   = c("PATIENT_ID" = "PATIENT_ID"),
+                  type = "INNER",
+                  cols = "CENTRAL_ID"
+               )
+            ),
+            raw_where = TRUE,
+            where     = glue(r"(
+         id_registry.PATIENT_ID IN (SELECT PATIENT_ID FROM ohasis_warehouse.id_registry WHERE CENTRAL_ID IN (SELECT CENTRAL_ID FROM ohasis_warehouse.id_registry WHERE PATIENT_ID IN ('{pids}')))
+            AND px_pii.{not_null} IS NOT NULL
+      )")
+         )
+         data_pid <- dbTable(lw_conn, "ohasis_lake", "px_pii", cols = c("PATIENT_ID", "SNAPSHOT", col), raw_where = TRUE, where = glue(r"(PATIENT_ID IN ('{pids}') AND px_pii.{not_null} IS NOT NULL)"))
+         dbDisconnect(lw_conn)
+
+         corr <- data_cid %>%
+            select(-PATIENT_ID) %>%
+            bind_rows(data_pid) %>%
+            mutate(
+               CENTRAL_ID = if_else(coalesce(CENTRAL_ID, "") == "", PATIENT_ID, CENTRAL_ID, CENTRAL_ID)
+            ) %>%
+            mutate_if(
+               .predicate = is.character,
+               ~clean_pii(.)
+            ) %>%
+            filter(!is.na({{col_name}})) %>%
+            arrange(desc(SNAPSHOT)) %>%
+            distinct(CENTRAL_ID, .keep_all = TRUE) %>%
+            select(-PATIENT_ID, -SNAPSHOT)
+
+         data %<>%
+            left_join(
+               y  = corr %>%
+                  rename(CORRECT_PII = {{col_name}}),
+               by = join_by(CENTRAL_ID)
+            ) %>%
+            mutate(
+               {{col_name}} := coalesce({{col_name}}, CORRECT_PII)
+            ) %>%
+            select(-CORRECT_PII)
+      }
+   }
+
+   return(data)
+}
