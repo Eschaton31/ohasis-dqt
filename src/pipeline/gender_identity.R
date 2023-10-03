09ac165513a3c9d2b063a5bd284712f1d9bc3797
jrpalo.doh <jrpalo@doh.gov.ph>
2023-10-03 23:48:49 +0800

    used new categorization

--- src/pipeline/gender_identity.R
+++ src/pipeline/gender_identity.R
@@ -3,7 +3,7 @@ generate_gender_identity <- function(linelist, sex, self_identity, self_identity
 
    col_sex <- deparse(substitute(sex))
    col_si  <- deparse(substitute(self_identity))
-   ref_gi  <- read_sheet("1LFYk4FGokmq4OzBsU6KW6cJWPUYKagXvR6VvvWu1uNo", "Sheet1", .name_repair = "unique_quiet")
+   ref_gi  <- read_sheet("1LFYk4FGokmq4OzBsU6KW6cJWPUYKagXvR6VvvWu1uNo", "curr_process", .name_repair = "unique_quiet")
    linelist %<>%
       # clean data
       mutate_at(
@@ -16,27 +16,29 @@ generate_gender_identity <- function(linelist, sex, self_identity, self_identity
             . %in% c("MALE", "MAN") ~ "MALE",
             . %in% c("FEMALE", "WOMAN") ~ "FEMALE",
             . %in% c("OTHER", "OTHERS") ~ "OTHERS",
+            . == "0" ~ NA_character_,
             TRUE ~ .
          )
       ) %>%
       mutate(
          {{self_identity_text}} := if_else({{self_identity}} == "OTHERS", {{self_identity_text}}, NA_character_, NA_character_),
          {{self_identity_text}} := toupper(str_squish({{self_identity_text}})),
-         SI_SIEVE               = if_else(
-            condition = !is.na({{self_identity_text}}),
-            true      = str_replace_all({{self_identity_text}}, "[^[:alnum:]]", ""),
-            false     = NA_character_
-         ),
+         SI_SIEVE               = str_replace_all({{self_identity_text}}, "[^[:alnum:]]", ""),
       ) %>%
       # gender identity
       left_join(
          y  = ref_gi %>%
-            mutate(SEX = remove_code(SEX)) %>%
+            mutate(
+               SEX        = toupper(remove_code(SEX)),
+               SELF_IDENT = toupper(remove_code(SELF_IDENT)),
+               SI_SIEVE   = str_replace_all(SELF_IDENT_OTHER, "[^[:alnum:]]", ""),
+            ) %>%
+            distinct(SEX, SELF_IDENT, SI_SIEVE, .keep_all = TRUE) %>%
             select(
                {{sex}}             := SEX,
-               {{self_identity}}   := self_identity,
-               SI_SIEVE            = self_identity_other_sieve,
-               {{gender_identity}} := gender_identity
+               {{self_identity}}   := SELF_IDENT,
+               SI_SIEVE,
+               {{gender_identity}} := correct_gi
             ),
          by = c(col_sex, col_si, "SI_SIEVE")
       ) %>%
