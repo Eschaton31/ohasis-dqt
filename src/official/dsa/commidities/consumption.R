1d2a161348ce5efa51d12442904b32cf65d11d60
jrpalo.doh <jrpalo@doh.gov.ph>
2024-04-27 21:39:10 +0800

    initial code for the inventory reports to dpcb

---
+++ src/official/dsa/commidities/consumption.R
@@ -0,0 +1,152 @@
+min <- "2023-01-01"
+max <- "2024-04-01"
+
+con               <- ohasis$conn("db")
+inventory_product <- QB$new(con)$from("ohasis_interim.inventory_product")$get()
+dbDisconnect(con)
+
+con       <- ohasis$conn("lw")
+disp_meds <- QB$new(con)$
+   from("ohasis_lake.disp_meds")$
+   join("ohasis_lake.px_pii AS rec", "disp_meds.REC_ID", "=", "rec.REC_ID")$
+   select("rec.PATIENT_ID",
+          "rec.FACI_ID AS RECORD_FACI",
+          "rec.SUB_FACI_ID AS RECORD_SUB_FACI",
+          "disp_meds.*")$
+   whereBetween("disp_meds.DISP_DATE", c(min, max))$
+   get()
+registry  <- QB$new(con)$from("ohasis_interim.registry")$get()
+dbDisconnect(con)
+
+tx <- read_dta(hs_data("harp_tx", "reg", 2023, 12))
+
+
+dispensing <- disp_meds %>%
+   get_cid(registry, PATIENT_ID) %>%
+   left_join(
+      y  = inventory_product %>%
+         select(MEDICINE = ITEM, COMMODITY = NAME, TYPICAL_BATCH),
+      by = join_by(MEDICINE)
+   ) %>%
+   mutate(
+      FACI_ID   = coalesce(FACI_ID, RECORD_FACI),
+      DISP_DATE = as.Date(DISP_DATE),
+      NEXT_DATE = as.Date(NEXT_DATE),
+      BOTTLES   = DISP_TOTAL / as.numeric(TYPICAL_BATCH)
+   ) %>%
+   ohasis$get_faci(
+      list(FACILITY = c("FACI_ID", "SUB_FACI_ID")),
+      "name",
+      c("REGION", "PROVINCE", "MUNCITY")
+   ) %>%
+   filter(
+      !is.na(COMMODITY),
+      DISP_DATE >= min,
+      DISP_DATE < max
+   )
+
+
+tx_2023 <- tx %>%
+   filter(artstart_date >= "2023-01-31") %>%
+   get_cid(registry, PATIENT_ID) %>%
+   left_join(
+      y  = dispensing %>%
+         select(REC_ID, REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY),
+      by = join_by(REC_ID)
+   ) %>%
+   mutate(
+      NEW_2023Q1 = if_else(artstart_date >= "2023-01-01" & artstart_date <= "2023-03-31", 1, 0, 0),
+      NEW_2023Q2 = if_else(artstart_date >= "2023-04-01" & artstart_date <= "2023-06-30", 1, 0, 0),
+      NEW_2023Q3 = if_else(artstart_date >= "2023-07-01" & artstart_date <= "2023-09-30", 1, 0, 0),
+      NEW_2023Q4 = if_else(artstart_date >= "2023-10-01" & artstart_date <= "2023-12-31", 1, 0, 0),
+   ) %>%
+   distinct(art_id, REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY, .keep_all = TRUE) %>%
+   group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+   summarise_at(
+      .vars = vars(NEW_2023Q1, NEW_2023Q2, NEW_2023Q3, NEW_2023Q4),
+      ~sum(.)
+   ) %>%
+   ungroup()
+
+disp_bottles <- dispensing %>%
+   group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+   summarise(
+      `DISP_202301-202403` = sum(BOTTLES, na.rm = TRUE),
+      DISP_2023Q1          = sum(if_else(DISP_DATE >= "2023-01-01" & DISP_DATE <= "2023-03-31", BOTTLES, 0, 0), na.rm = TRUE),
+      DISP_2023Q2          = sum(if_else(DISP_DATE >= "2023-04-01" & DISP_DATE <= "2023-06-30", BOTTLES, 0, 0), na.rm = TRUE),
+      DISP_2023Q3          = sum(if_else(DISP_DATE >= "2023-07-01" & DISP_DATE <= "2023-09-30", BOTTLES, 0, 0), na.rm = TRUE),
+      DISP_2023Q4          = sum(if_else(DISP_DATE >= "2023-10-01" & DISP_DATE <= "2023-12-31", BOTTLES, 0, 0), na.rm = TRUE),
+      DISP_2024Q1          = sum(if_else(DISP_DATE >= "2024-01-01" & DISP_DATE <= "2024-03-31", BOTTLES, 0, 0), na.rm = TRUE),
+   ) %>%
+   ungroup() %>%
+   left_join(
+      y  = dispensing %>%
+         distinct(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY, CENTRAL_ID) %>%
+         group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+         summarise(
+            `CLIENTS_202301-202403` = n()
+         ) %>%
+         ungroup(),
+      by = join_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY)
+   ) %>%
+   left_join(
+      y  = dispensing %>%
+         filter(DISP_DATE >= "2023-01-01", DISP_DATE <= "2023-03-31") %>%
+         distinct(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY, CENTRAL_ID) %>%
+         group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+         summarise(
+            CLIENTS_2023Q1 = n()
+         ) %>%
+         ungroup(),
+      by = join_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY)
+   ) %>%
+   left_join(
+      y  = dispensing %>%
+         filter(DISP_DATE >= "2023-04-01", DISP_DATE <= "2023-06-30") %>%
+         distinct(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY, CENTRAL_ID) %>%
+         group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+         summarise(
+            CLIENTS_2023Q2 = n()
+         ) %>%
+         ungroup(),
+      by = join_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY)
+   ) %>%
+   left_join(
+      y  = dispensing %>%
+         filter(DISP_DATE >= "2023-07-01", DISP_DATE <= "2023-09-30") %>%
+         distinct(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY, CENTRAL_ID) %>%
+         group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+         summarise(
+            CLIENTS_2023Q3 = n()
+         ) %>%
+         ungroup(),
+      by = join_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY)
+   ) %>%
+   left_join(
+      y  = dispensing %>%
+         filter(DISP_DATE >= "2023-10-01", DISP_DATE <= "2023-12-31") %>%
+         distinct(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY, CENTRAL_ID) %>%
+         group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+         summarise(
+            CLIENTS_2023Q4 = n()
+         ) %>%
+         ungroup(),
+      by = join_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY)
+   ) %>%
+   left_join(
+      y  = dispensing %>%
+         filter(DISP_DATE >= "2024-01-01", DISP_DATE <= "2024-03-31") %>%
+         distinct(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY, CENTRAL_ID) %>%
+         group_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY) %>%
+         summarise(
+            CLIENTS_2024Q1 = n()
+         ) %>%
+         ungroup(),
+      by = join_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY)
+   ) %>%
+   left_join(
+      y  = tx_2023,
+      by = join_by(REGION, PROVINCE, MUNCITY, FACILITY, COMMODITY)
+   )
+
+write_sheet(disp_bottles, "19MRpTfgAeNQ14EVeG23MWu3wuLM6lcE_wA5YwmYSbKA", "disp_bottles")
---
+++ src/official/dsa/mir/commodities.R
@@ -0,0 +1,178 @@
+con                <- ohasis$conn("db")
+inventory          <- QB$new(con)$from("ohasis_interim.inventory")$get()
+inventory          <- QB$new(con)$from("ohasis_interim.inventory")$get()
+inventory_product  <- QB$new(con)$from("ohasis_interim.inventory_product")$get()
+inventory_transact <- QB$new(con)$
+   from("ohasis_interim.inventory_transact AS trxn")$
+   join("ohasis_interim.px_record AS rec", "trxn.TRANSACT_ID", "=", "rec.REC_ID")$
+   whereBetween('rec.RECORD_DATE', c("2024-01-01", "2024-03-31"))$
+   select("rec.PATIENT_ID", "trxn.*")$
+   get()
+registry           <- QB$new(con)$from("ohasis_interim.registry")$get()
+dbDisconnect(con)
+
+###
+
+inv <- inventory %>%
+   filter(ITEM_CURR >= 0) %>%
+   left_join(
+      y  = inventory_product %>%
+         select(ITEM_ID = ITEM, COMMODITY = NAME, TYPICAL_BATCH),
+      by = join_by(ITEM_ID)
+   ) %>%
+   mutate(
+      CATEGORY = case_when(
+         str_detect(ITEM_ID, "^1") ~ "Diagnostics",
+         str_detect(ITEM_ID, "^2") ~ "Medicines",
+      )
+   ) %>%
+   ohasis$get_faci(
+      list(FACILITY = c("FACI_ID", "SUB_FACI_ID")),
+      "name",
+      c("REGION", "PROVINCE", "MUNCITY")
+   )
+
+current_stocks <- inv %>%
+   group_by(CATEGORY, COMMODITY, REGION, FACILITY) %>%
+   summarise(
+      CURRENT_STATUS = sum(ITEM_CURR, na.rm = TRUE)
+   ) %>%
+   ungroup()
+
+expiry <- inv %>%
+   mutate(
+      EXPIRE_DATE = format(EXPIRE_DATE, "%Y-%m")
+   ) %>%
+   group_by(CATEGORY, COMMODITY, REGION, FACILITY, EXPIRE_DATE) %>%
+   summarise(
+      STOCK_ON_HAND = sum(ITEM_CURR, na.rm = TRUE)
+   ) %>%
+   ungroup() %>%
+   pivot_wider(
+      id_cols     = c(CATEGORY, COMMODITY, REGION, FACILITY),
+      names_from  = EXPIRE_DATE,
+      values_from = STOCK_ON_HAND
+   )
+
+transactions <- inventory_transact %>%
+   inner_join(
+      y  = inv %>%
+         select(INVENTORY_ID, CATEGORY, COMMODITY, REGION, FACILITY, TYPICAL_BATCH),
+      by = join_by(INVENTORY_ID)
+   ) %>%
+   mutate(
+      TRANSACT_TOTAL = case_when(
+         UNIT_BASIS == 1 ~ TRANSACT_QUANTITY * as.numeric(TYPICAL_BATCH),
+         TRUE ~ TRANSACT_QUANTITY
+      )
+   )
+
+period <- transactions %>%
+   group_by(CATEGORY, COMMODITY, REGION, FACILITY) %>%
+   summarise(
+      DISPENSED_PERIOD = sum(TRANSACT_QUANTITY, na.rm = TRUE)
+   ) %>%
+   ungroup()
+
+clients <- transactions %>%
+   get_cid(registry, PATIENT_ID) %>%
+   distinct(CATEGORY, COMMODITY, REGION, FACILITY, CENTRAL_ID) %>%
+   group_by(CATEGORY, COMMODITY, REGION, FACILITY) %>%
+   summarise(
+      UNIQUE_CLIENTS = n()
+   ) %>%
+   ungroup()
+
+status <- current_stocks %>%
+   left_join(expiry) %>%
+   left_join(period) %>%
+   left_join(clients)
+
+write_sheet(status, "19MRpTfgAeNQ14EVeG23MWu3wuLM6lcE_wA5YwmYSbKA", "stock_status")
+
+### COnfirmatory
+
+ con     <- ohasis$conn("lw")
+pending <- QB$new(con)$
+   from("ohasis_lake.px_hiv_testing AS test")$
+   join("ohasis_lake.px_pii AS pii", "test.REC_ID", "=", "pii.REC_ID")$
+   leftJoin("ohasis_warehouse.id_registry AS reg", "pii.PATIENT_ID", "=", "reg.PATIENT_ID")$
+   where("CONFIRM_RESULT", "regexp", "Pending")$
+   select(pii.FACI_ID,
+          pii.SUB_FACI_ID,
+          pii.RECORD_DATE,
+          pii.DISEASE,
+          pii.MODULE,
+          pii.PRIME,
+          pii.CONFIRMATORY_CODE,
+          pii.UIC,
+          pii.PHILHEALTH_NO,
+          pii.SEX,
+          pii.BIRTHDATE,
+          pii.PATIENT_CODE,
+          pii.PHILSYS_ID,
+          pii.FIRST,
+          pii.MIDDLE,
+          pii.LAST,
+          pii.SUFFIX,
+          test.CONFIRM_FACI,
+          test.CONFIRM_SUB_FACI,
+          test.CONFIRM_TYPE,
+          test.CONFIRM_CODE,
+          test.SPECIMEN_REFER_TYPE,
+          test.SPECIMEN_SOURCE,
+          test.SPECIMEN_SUB_SOURCE,
+          test.CONFIRM_RESULT,
+          test.CONFIRM_REMARKS,
+          test.SIGNATORY_1,
+          test.SIGNATORY_2,
+          test.SIGNATORY_3,
+          test.DATE_RELEASE,
+          test.DATE_CONFIRM,
+          test.IDNUM,
+          test.T0_DATE,
+          test.T0_RESULT,
+          test.T1_DATE,
+          test.T1_KIT,
+          test.T1_RESULT,
+          test.T2_DATE,
+          test.T2_KIT,
+          test.T2_RESULT,
+          test.T3_DATE,
+          test.T3_KIT,
+          test.T3_RESULT)$
+   selectRaw("COALESCE(reg.CENTRAL_ID, pii.PATIENT_ID) AS CENTRAL_ID")$
+   get()
+pos     <- QB$new(con)$
+   from("harp_dx.reg_202312 AS dx")$
+   leftJoin("ohasis_warehouse.id_registry AS reg", "dx.PATIENT_ID", "=", "reg.PATIENT_ID")$
+   select(dx.PATIENT_ID,
+          dx.labcode2,
+          dx.confirm_date,
+          dx.firstname,
+          dx.middle,
+          dx.last,
+          dx.name_suffix,
+          dx.bdate,
+          dx.sex,
+          dx.dx_region,
+          dx.dx_province,
+          dx.dx_muncity,
+          dx.dxlab_standard)$
+   selectRaw("COALESCE(reg.CENTRAL_ID, dx.PATIENT_ID) AS CENTRAL_ID")$
+   get()
+dbDisconnect(con)
+
+
+pending_pos <- pending %>%
+   inner_join(
+      y  = pos %>%
+         select(CENTRAL_ID,
+                labcode2,
+                confirm_date,
+                dx_region,
+                dx_province,
+                dx_muncity,
+                dxlab_standard),
+      by = join_by(CENTRAL_ID)
+   )
