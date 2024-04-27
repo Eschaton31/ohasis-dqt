9473ba47eff3a171afb49d70bb3f958904277fbd
jrpalo.doh <jrpalo@doh.gov.ph>
2024-04-27 21:45:47 +0800

    added update_visits to harp_dx pipeline

--- main.R
+++ main.R
@@ -16,8 +16,8 @@ ohasis <- DB$new("2024", "01", "finalize harp", "2")
 
 ##  HARP Diagnosis -------------------------------------------------------------
 
-harp_dx$steps$`01_load_reqs`$.init(harp_dx, end_date = end_ym(ohasis$yr, ohasis$mo), dl_corr = "1", dl_forms = "1", update_lw = "2", update_harp = "1", harp_reprocess = "1")
-harp_dx$steps$`02_data_hts_tst_pos`$.init(harp_dx, run_checks = "2", upload = "1", exclude_drops = "1", save = "1")
+harp_dx$steps$`01_load_reqs`$.init(harp_dx, end_date = end_ym(ohasis$yr, ohasis$mo), dl_corr = "1", dl_forms = "1", update_lw = "2", update_visits = "1", update_harp = "1", harp_reprocess = "1")
+harp_dx$steps$`02_data_hts_tst_pos`$.init(harp_dx, run_checks = "1", upload = "1", exclude_drops = "1", save = "1")
 harp_dx$steps$x1_dedup_new$.init(harp_dx, upload = "1")
 harp_dx$steps$x2_dedup_old$.init(harp_dx, upload = "1")
 
@@ -28,9 +28,9 @@ harp_dx$steps$y3_saccl_recency$import_data(harp_dx$steps$y3_saccl_recency$tables
 
 ##  HARP Treatment -------------------------------------------------------------
 
-harp_tx$steps$`01_load_reqs`$.init(harp_tx, end_date = end_ym(ohasis$yr, ohasis$mo), dl_corr = "1", dl_forms = "1", update_lw = "2", update_visits = "1", update_harp = "1", harp_reprocess = "1")
-harp_tx$steps$`02_data_tx_new`$.init(harp_tx, run_checks = "2", upload = "1", exclude_drops = "1")
-harp_tx$steps$`03_data_tx_curr`$.init(harp_tx, run_checks = "2", upload = "1", save = "1")
+harp_tx$steps$`01_load_reqs`$.init(harp_tx, end_date = end_ym(ohasis$yr, ohasis$mo), dl_corr = "1", dl_forms = "1", update_lw = "1", update_visits = "1", update_harp = "1", harp_reprocess = "1")
+harp_tx$steps$`02_data_tx_new`$.init(harp_tx, run_checks = "1", upload = "1", exclude_drops = "1")
+harp_tx$steps$`03_data_tx_curr`$.init(harp_tx, run_checks = "1", upload = "1", save = "1")
 harp_tx$steps$x1_dedup_new$.init(harp_tx, upload = "1")
 harp_tx$steps$x2_dedup_old$.init(harp_tx, upload = "1")
 harp_tx$steps$x3_dedup_dx$.init(harp_tx, upload = "1")
@@ -58,6 +58,5 @@ prep$steps$x4_dedup_tx$.init(prep, upload = "1")
 recency$steps$`01_load_reqs`$.init(recency, get_harp = "1", dl_forms = "1", update_lw = "1", update_rt = "1")
 recency$steps$`02_data_hts_recent`$.init(recency, run_checks = "2", upload = "1", save = "2")
 recency$steps$`03_upload`$.init(recency)
-
 
 ## END -------------------------------------------------------------------------
