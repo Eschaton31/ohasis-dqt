938fbf59b58ce48da78789fcb26f44432649e874
jrpalo.doh <jrpalo@doh.gov.ph>
2024-04-27 21:32:48 +0800

    added some code to create a unique pii dataset

--- src/misc/dedup_ohasis/00_main.R
+++ src/misc/dedup_ohasis/00_main.R
@@ -3,6 +3,7 @@
 source("src/misc/dedup_ohasis/01_pii.R")
 source("src/misc/dedup_ohasis/02_dedup_fns.R")
 
+ohasis$data_factory("lake", "px_pii", "upsert", TRUE, to = format(Sys.time(), "%Y-%m-%d %H:%M:%S"))
 ohasis$data_factory("warehouse", "id_registry", "upsert", TRUE, to = format(Sys.time(), "%Y-%m-%d %H:%M:%S"))
 dedup <- dedup_download()
 dedup <- dedup_linelist(dedup)
@@ -111,22 +112,111 @@ upload_dupes(check_dupes$normal_up)
 # new process
 dedup$id_registry <- upload_dupes2(check_dupes$registry_up, dedup$id_registry)
 dedup$id_registry <- upload_dupes2(check_dupes$normal_up, dedup$id_registry)
-dedup <- dedup_linelist2(dedup)
+dedup             <- dedup_linelist2(dedup)
 
-from <- "2024-01-28 12:30:00"
+from              <- "2024-04-27 15:55:00"
 dedup$id_registry <- upload_dupes2(check_dupes$normal_up, dedup$id_registry, TRUE, from)
 
 check_dupes$registry %>%
    mutate(
-      type = "reg",
+      type    = "reg",
       .before = 1
    ) %>%
    bind_rows(
       check_dupes$normal %>%
          mutate(
-            type = "norm",
+            type    = "norm",
             .before = 1
          )
    ) %>%
    select(-PATIENT_ID) %>%
    View()
+
+ohasis$data_factory("lake", "px_pii", "upsert", TRUE, to = format(Sys.time(), "%Y-%m-%d %H:%M:%S"))
+ohasis$data_factory("warehouse", "id_registry", "upsert", TRUE, to = format(Sys.time(), "%Y-%m-%d %H:%M:%S"))
+dedup      <- dedup_download()
+pii_unique <- dedup$pii %>%
+   select(-REC_ID, -FACI_ID, -SUB_FACI_ID, -DELETED_AT) %>%
+   pivot_longer(
+      cols = c(
+         FIRST,
+         MIDDLE,
+         LAST,
+         SUFFIX,
+         UIC,
+         CONFIRMATORY_CODE,
+         PATIENT_CODE,
+         BIRTHDATE,
+         PHILSYS_ID,
+         PHILHEALTH_NO,
+         CLIENT_EMAIL,
+         CLIENT_MOBILE,
+         SEX,
+         PERM_REG,
+         PERM_PROV,
+         PERM_MUNC,
+         CURR_REG,
+         CURR_PROV,
+         CURR_MUNC,
+      )
+   ) %>%
+   mutate(
+      sort = if_else(!is.na(value), 1, 9999, 9999)
+   ) %>%
+   arrange(sort, desc(SNAPSHOT)) %>%
+   distinct(PATIENT_ID, name, .keep_all = TRUE) %>%
+   pivot_wider(
+      id_cols     = PATIENT_ID,
+      names_from  = name,
+      values_from = value
+   ) %>%
+   get_cid(dedup$id_registry, PATIENT_ID)
+
+
+lw_conn <- ohasis$conn("lw")
+dbExecute(lw_conn, "DELETE FROM ohasis_lake.pii_unique WHERE PATIENT_ID IS NOT NULL;")
+ohasis$upsert(lw_conn, "lake", "pii_unique", pii_unique, "PATIENT_ID")
+dbDisconnect(lw_conn)
+
+change_px_id('2024012814706OJ1300000048', '20220120130001D574', "2022071413000173V3")
+
+distinct_pii <- function(cid1, cid2) {
+   data <- dedup$pii %>%
+      select(
+         CENTRAL_ID,
+         PATIENT_ID,
+         FIRST,
+         MIDDLE,
+         LAST,
+         SUFFIX,
+         UIC,
+         CONFIRMATORY_CODE,
+         PATIENT_CODE,
+         BIRTHDATE,
+         PHILSYS_ID,
+         PHILHEALTH_NO,
+         CLIENT_EMAIL,
+         CLIENT_MOBILE,
+         SEX,
+         PERM_REG,
+         PERM_PROV,
+         PERM_MUNC,
+         CURR_REG,
+         CURR_PROV,
+         CURR_MUNC,
+      )
+
+   return(
+      list(
+         p1 = data %>% filter(CENTRAL_ID == cid1),
+         p2 = data %>% filter(CENTRAL_ID == cid2)
+      )
+   )
+}
+
+lapply(
+   c('20240104101246Q0400030011'),
+   change_px_id,
+   'HARP08011816074489',
+   'HARP08011816049826'
+)
