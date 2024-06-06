48f8955827753ffd05ea7df836cef769f961cb77
bene.palo <jrpalo@doh.gov.ph>
2024-06-06 15:56:43 +0800

    analyses for ly requests

--- src/misc/ly_extracts/hts.R
+++ src/misc/ly_extracts/hts.R
@@ -70,9 +70,77 @@ ly_test <- testing %>%
          hts_result != "(no data)" & hts_modality == "FBS" ~ 5,
          hts_result != "(no data)" & hts_modality == "ST" ~ 6,
          TRUE ~ 9999
-      )
+      ),
+
+      AGE          = coalesce(AGE, calc_age(BIRTHDATE, RECORD_DATE)),
+      AGE_BAND     = case_when(
+         AGE %between% c(0, 14) ~ "1) <15",
+         AGE %between% c(15, 17) ~ "2) 15-17",
+         AGE %between% c(18, 24) ~ "3) 18-24",
+         AGE %between% c(25, 34) ~ "4) 24-34",
+         AGE %between% c(35, 49) ~ "5) 35-49",
+         AGE %between% c(50, 1000) ~ "6) 50+",
+         TRUE ~ "(no data)"
+      ),
+
+      drop         = if_all(c(FIRST, LAST, BIRTHDATE, SEX), ~is.na(.)),
+
+      KP_MSM       = if_else(SEX == "1_Male" & str_detect(risk_sexwithm, "yes"), "MSM", NA_character_),
+      KP_TG        = case_when(
+         SEX == "1_Male" & StrLeft(SELF_IDENT, 1) %in% c("2", "3") ~ "TGW",
+         SEX == "2_Female" & StrLeft(SELF_IDENT, 1) %in% c("1", "3") ~ "TGM",
+         TRUE ~ NA_character_
+      ),
+      KP_PDL       = case_when(
+         StrLeft(CLIENT_TYPE, 1) == "7" ~ "PDL",
+         TRUE ~ NA_character_
+      ),
+      KP_SW        = case_when(
+         str_detect(risk_paymentforsex, "yes") ~ "SW",
+         str_detect(COUNSEL_NOTES, "SEX WORKER") ~ "SW",
+         TRUE ~ NA_character_
+      ),
+      KP_PWUD      = case_when(
+         str_detect(risk_illicitdrug, "yes") ~ "PWUD",
+         str_detect(risk_injectdrug, "yes") ~ "PWUD",
+         TRUE ~ NA_character_
+      ),
+
+      hts_result   = coalesce(hts_result, "NR")
+   ) %>%
+   unite(
+      col   = "KAP_TYPE",
+      sep   = "-",
+      starts_with("KP_", ignore.case = FALSE),
+      na.rm = TRUE
+   ) %>%
+   mutate(
+      KAP_TYPE = coalesce(na_if(KAP_TYPE, ""), "No apparent risk")
    ) %>%
    arrange(site_sort, hts_priority) %>%
+   mutate_at(
+      .vars = vars(CURR_REG, CURR_PROV, CURR_MUNC, PERM_REG, PERM_PROV, PERM_MUNC),
+      ~coalesce(., "Unknown")
+   ) %>%
    distinct(CENTRAL_ID, hts_date, hts_modality, .keep_all = TRUE)
+
+ly_test %>%
+   filter(!drop) %>%
+   group_by(KAP_TYPE) %>%
+   summarise(
+      Tested         = n(),
+      `Non-reactive` = sum(if_else(hts_result == "NR", 1, 0, 0)),
+      Reactive       = sum(if_else(hts_result == "R", 1, 0, 0)),
+   ) %>%
+   ungroup() %>%
+   adorn_totals() %>%
+   mutate(
+      RR = Reactive / Tested
+   ) %>%
+   write_clip()
+summarise()
+ly_test %>%
+   filter(!drop) %>%
+   tab(SEX)
 
 write_dta(format_stata(testing), "D:/20240419_hts-ly_20231216-20240331.dta")
--- src/misc/ly_extracts/prep.R
+++ src/misc/ly_extracts/prep.R
@@ -58,3 +58,245 @@ ly_prep %>%
       earliest = min(VISIT_DATE, na.rm = TRUE),
       latest   = max(VISIT_DATE, na.rm = TRUE),
    )
+
+lw_conn <- ohasis$conn("lw")
+dx      <- QB$new(lw_conn)$from("harp_dx.reg_202403")$get()
+tx_reg  <- QB$new(lw_conn)$from("harp_tx.reg_202403")$get()
+tx_out  <- QB$new(lw_conn)$from("harp_tx.outcome_202403")$get()
+dbDisconnect(lw_conn)
+
+ly_art_enroll <- tx_reg %>%
+   filter(year(artstart_date) == 2024, artstart_hub == "TLY") %>%
+   faci_code_to_id(
+      ohasis$ref_faci_code,
+      list(FACI_ID = "artstart_hub", SUB_FACI_ID = "artstart_branch")
+   ) %>%
+   ohasis$get_faci(
+      list(site_name = c("FACI_ID", "SUB_FACI_ID")),
+      "name",
+   ) %>%
+   left_join(
+      y  = dx %>%
+         select(
+            idnum,
+            reg_sex = sex,
+            transmit,
+            self_identity,
+            sexhow,
+            region,
+            province,
+            muncity
+         ),
+      by = join_by(idnum)
+   ) %>%
+   mutate(
+      # KAP
+      msm            = case_when(
+         reg_sex == "MALE" & sexhow %in% c("BISEXUAL", "HOMOSEXUAL") ~ 1,
+         reg_sex == "MALE" & sexhow == "HETEROSEXUAL" ~ 0,
+         TRUE ~ 0
+      ),
+      tg             = case_when(
+         reg_sex == "MALE" & self_identity %in% c("FEMALE", "OTHERS") ~ 1,
+         reg_sex == "FEMALE" & self_identity %in% c("MALE", "OTHERS") ~ 1,
+         TRUE ~ 0
+      ),
+      hetero         = if_else(
+         condition = sexhow == "HETEROSEXUAL",
+         true      = 1,
+         false     = 0,
+         missing   = 0
+      ),
+      pwid           = if_else(
+         condition = transmit == "IVDU",
+         true      = 1,
+         false     = 0,
+         missing   = 0
+      ),
+      unknown        = case_when(
+         transmit == "UNKNOWN" ~ 1,
+         is.na(transmit) ~ 1,
+         TRUE ~ 0
+      ),
+      Key_Population = case_when(
+         msm == 1 ~ "MSM",
+         reg_sex == "MALE" & tg == 1 ~ "TGW",
+         reg_sex == "FEMALE" & tg == 1 ~ "TGM",
+         pwid == 1 ~ "PWID",
+         transmit == "SEX" & sex == "MALE" ~ "Hetero Male",
+         transmit == "SEX" & sex == "FEMALE" ~ "Hetero Female",
+         unknown == 1 ~ "Unknown",
+         TRUE ~ "Others"
+      ),
+
+
+      AGE            = age,
+      AGE            = case_when(
+         art_id == 108586 ~ 20,
+         TRUE ~ AGE
+      ),
+      AGE_BAND       = case_when(
+         AGE %between% c(0, 14) ~ "1) <15",
+         AGE %between% c(15, 17) ~ "2) 15-17",
+         AGE %between% c(18, 24) ~ "3) 18-24",
+         AGE %between% c(25, 34) ~ "4) 25-34",
+         AGE %between% c(35, 49) ~ "5) 35-49",
+         AGE %between% c(50, 1000) ~ "6) 50+",
+         TRUE ~ "(no data)"
+      ),
+   ) %>%
+   harp_addr_to_id(
+      ohasis$ref_addr,
+      c(
+         PERM_PSGC_REG  = "region",
+         PERM_PSGC_PROV = "province",
+         PERM_PSGC_MUNC = "muncity"
+      )
+   ) %>%
+   ohasis$get_addr(
+      c(
+         PERM_NAME_REG  = "PERM_PSGC_REG",
+         PERM_NAME_PROV = "PERM_PSGC_PROV",
+         PERM_NAME_MUNC = "PERM_PSGC_MUNC"
+      ),
+      "name"
+   )
+ly_art_curr   <- tx_out %>%
+   filter(realhub == "TLY") %>%
+   faci_code_to_id(
+      ohasis$ref_faci_code,
+      list(FACI_ID = "realhub", SUB_FACI_ID = "realhub_branch")
+   ) %>%
+   ohasis$get_faci(
+      list(site_name = c("FACI_ID", "SUB_FACI_ID")),
+      "name",
+   ) %>%
+   left_join(
+      y  = dx %>%
+         select(
+            idnum,
+            reg_sex = sex,
+            transmit,
+            self_identity,
+            sexhow,
+            region,
+            province,
+            muncity
+         ),
+      by = join_by(idnum)
+   ) %>%
+   mutate(
+      # KAP
+      msm            = case_when(
+         reg_sex == "MALE" & sexhow %in% c("BISEXUAL", "HOMOSEXUAL") ~ 1,
+         reg_sex == "MALE" & sexhow == "HETEROSEXUAL" ~ 0,
+         TRUE ~ 0
+      ),
+      tg             = case_when(
+         reg_sex == "MALE" & self_identity %in% c("FEMALE", "OTHERS") ~ 1,
+         reg_sex == "FEMALE" & self_identity %in% c("MALE", "OTHERS") ~ 1,
+         TRUE ~ 0
+      ),
+      hetero         = if_else(
+         condition = sexhow == "HETEROSEXUAL",
+         true      = 1,
+         false     = 0,
+         missing   = 0
+      ),
+      pwid           = if_else(
+         condition = transmit == "IVDU",
+         true      = 1,
+         false     = 0,
+         missing   = 0
+      ),
+      unknown        = case_when(
+         transmit == "UNKNOWN" ~ 1,
+         is.na(transmit) ~ 1,
+         TRUE ~ 0
+      ),
+      Key_Population = case_when(
+         msm == 1 ~ "MSM",
+         reg_sex == "MALE" & tg == 1 ~ "TGW",
+         reg_sex == "FEMALE" & tg == 1 ~ "TGM",
+         pwid == 1 ~ "PWID",
+         transmit == "SEX" & sex == "MALE" ~ "Hetero Male",
+         transmit == "SEX" & sex == "FEMALE" ~ "Hetero Female",
+         unknown == 1 ~ "Unknown",
+         TRUE ~ "Others"
+      ),
+
+
+      AGE            = curr_age,
+      AGE_BAND       = case_when(
+         AGE %between% c(0, 14) ~ "1) <15",
+         AGE %between% c(15, 17) ~ "2) 15-17",
+         AGE %between% c(18, 24) ~ "3) 18-24",
+         AGE %between% c(25, 34) ~ "4) 24-34",
+         AGE %between% c(35, 49) ~ "5) 35-49",
+         AGE %between% c(50, 1000) ~ "6) 50+",
+         TRUE ~ "(no data)"
+      ),
+   ) %>%
+   harp_addr_to_id(
+      ohasis$ref_addr,
+      c(
+         PERM_PSGC_REG  = "region",
+         PERM_PSGC_PROV = "province",
+         PERM_PSGC_MUNC = "muncity"
+      )
+   ) %>%
+   ohasis$get_addr(
+      c(
+         PERM_NAME_REG  = "PERM_PSGC_REG",
+         PERM_NAME_PROV = "PERM_PSGC_PROV",
+         PERM_NAME_MUNC = "PERM_PSGC_MUNC"
+      ),
+      "name"
+   )
+
+ly_art_enroll %>%
+   group_by(AGE_BAND) %>%
+   summarise(
+      Enrolled = n()
+   ) %>%
+   adorn_totals() %>%
+   write_clip()
+
+ly_art_curr %>%
+   group_by(PERM_NAME_REG, PERM_NAME_PROV, PERM_NAME_MUNC, outcome) %>%
+   summarise(
+      Clients = n()
+   ) %>%
+   pivot_wider(
+      values_from = Clients,
+      names_from  = outcome
+   ) %>%
+   adorn_totals() %>%
+   write_clip()
+
+ly_art_curr %>%
+   mutate(
+      vl_tested = if_else(outcome == "alive on arv" &
+                             is.na(baseline_vl) &
+                             !is.na(vlp12m), 1, 0, 0),
+      vl_ud     = if_else(outcome == "alive on arv" &
+                             is.na(baseline_vl) &
+                             vlp12m == 1, 1, 0, 0),
+      vl_sup    = if_else(outcome == "alive on arv" &
+                             is.na(baseline_vl) &
+                             !is.na(vlp12m) &
+                             vl_result < 1000, 1, 0, 0),
+   ) %>%
+   group_by(PERM_NAME_REG, PERM_NAME_PROV, PERM_NAME_MUNC) %>%
+   summarise(
+      `alive on arv` = sum(if_else(outcome == "alive on arv", 1, 0)),
+      vl_tested_p12m = sum(vl_tested),
+      vl_sup         = sum(vl_sup),
+      vl_ud          = sum(vl_ud),
+   ) %>%
+   adorn_totals() %>%
+   mutate(
+      `suppression rate (among tested)`  = vl_sup / vl_tested_p12m,
+      `undetectable rate (among tested)` = vl_ud / vl_tested_p12m,
+   ) %>%
+   write_clip()
