40a545932be338cb58c6e38ab3ffb807a0fcb733
John Benedict Palo <jrpalo@doh.gov.ph>
2024-11-20 12:21:57 +0800

    converged methods for inheritance

--- src/classes/OhasisData.R
+++ src/classes/OhasisData.R
@@ -1,32 +1,31 @@
 OhasisData <- R6Class(
    "OhasisData",
    public  = list(
-      periods    = list(
+      periods      = list(
          curr  = c(
-            yr  = NULL,
-            mo  = NULL,
-            ym  = NULL,
+            yr  = NA_integer_,
+            mo  = NA_integer_,
+            ym  = NA_character_,
             min = NA_Date_,
             max = NA_Date_
          ),
          prev  = c(
-            yr  = NULL,
-            mo  = NULL,
-            ym  = NULL,
+            yr  = NA_integer_,
+            mo  = NA_integer_,
+            ym  = NA_character_,
             min = NA_Date_,
             max = NA_Date_
          ),
          after = c(
-            yr  = NULL,
-            mo  = NULL,
-            ym  = NULL,
+            yr  = NA_integer_,
+            mo  = NA_integer_,
+            ym  = NA_character_,
             min = NA_Date_,
             max = NA_Date_
          )
       ),
-      refs       = list(),
 
-      initialize = function(end_date) {
+      initialize   = function(end_date) {
          if (missing(end_date)) {
             end_date <- Sys.Date()
          }
@@ -40,71 +39,50 @@ OhasisData <- R6Class(
          self$periods$after <- private$calculatePeriod(end_date %m+% months(1))
       },
 
-      fetchRefs  = function() {
-         log_info("Downloading references.")
+      updateForms  = function(from = NULL, to = NULL) {
+         lapply(private$tables$lake, ohasis$data_factory, db_type = "lake", update_type = "upsert", default_yes = TRUE, from = from, to = to)
+         lapply(private$tables$warehouse, ohasis$data_factory, db_type = "warehouse", update_type = "upsert", default_yes = TRUE, from = from, to = to)
 
-         db_conn               <- connect("ohasis-live")
-         self$refs$ref_country <- QB$new(db_conn)$from("ohasis_interim.addr_country")$get()
-         dbDisconnect(db_conn)
+         invisible(self)
+      },
 
-         lw_conn             <- connect("ohasis-lw")
-         self$refs$addr      <- QB$new(lw_conn)$from("ohasis_lake.ref_addr")$get()
-         self$refs$staff     <- QB$new(lw_conn)$from("ohasis_lake.ref_staff")$get()
-         self$refs$faci      <- QB$new(lw_conn)$from("ohasis_lake.ref_faci")$get()
-         self$refs$faci_code <- self$refs$faci %>%
-            filter(!is.na(FACI_CODE)) %>%
-            mutate(
-               branch_priority = case_when(
-                  FACI_ID == "130001" ~ 1,
-                  FACI_ID == "130605" ~ 2,
-                  FACI_ID == "130748" ~ 3,
-                  TRUE ~ 9999
-               )
-            ) %>%
-            arrange(branch_priority) %>%
-            distinct(FACI_CODE, .keep_all = TRUE) %>%
-            rename(
-               SUB_FACI_CODE = FACI_CODE
-            ) %>%
-            mutate(
-               FACI_CODE     = case_when(
-                  stri_detect_regex(SUB_FACI_CODE, "^HASH") ~ "HASH",
-                  stri_detect_regex(SUB_FACI_CODE, "^SAIL") ~ "SAIL",
-                  stri_detect_regex(SUB_FACI_CODE, "^TLY") ~ "TLY",
-                  TRUE ~ SUB_FACI_CODE
-               ),
-               SUB_FACI_CODE = if_else(
-                  condition = nchar(SUB_FACI_CODE) == 3,
-                  true      = NA_character_,
-                  false     = SUB_FACI_CODE
-               ),
-               SUB_FACI_CODE = case_when(
-                  FACI_CODE == "HASH" & is.na(SUB_FACI_CODE) ~ "HASH-QC",
-                  FACI_CODE == "TLY" & is.na(SUB_FACI_CODE) ~ "TLY-ANGLO",
-                  FACI_CODE == "SHP" & is.na(SUB_FACI_CODE) ~ "SHIP-MAKATI",
-                  TRUE ~ SUB_FACI_CODE
-               ),
-            ) %>%
-            relocate(FACI_CODE, SUB_FACI_CODE, .before = 1)
+      tableFromSql = function(table, path) {
+         log_info("Processing {green(table)}.")
 
+         lw_conn     <- connect("ohasis-lw")
+         db_name     <- "ohasis_warehouse"
+         table_space <- Id(schema = db_name, table = table)
+
+         # update lake
+         if (dbExistsTable(lw_conn, table_space)) dbRemoveTable(lw_conn, table_space)
+
+         create <- stri_c("CREATE TABLE ", db_name, ".", table, " AS ")
+         select <- read_file(file.path(path, stri_c(table, ".sql")))
+
+         dbExecute(
+            lw_conn,
+            stri_c(create, select)
+         )
+
+         log_success("Done!")
          dbDisconnect(lw_conn)
 
-         log_success("Done.")
          invisible(self)
       }
    ),
    private = list(
       calculatePeriod = function(end_date) {
-         max <- end_date
-         min <- floor_date(max, "months")
-         yr  <- year(max)
-         mo  <- month(max)
-         ym  <- format(max, "%Y.%m")
+         yr  <- year(end_date)
+         mo  <- month(end_date)
+         ym  <- format(end_date, "%Y.%m")
+         min <- as.character(floor_date(end_date, "months"))
+         max <- as.character(end_date)
 
          return(c(yr = yr, mo = mo, ym = ym, min = min, max = max))
-      }
+      },
+      tables          = list(
+         lake      = list(),
+         warehouse = list()
+      )
    )
 )
-
-harp <- OhasisData$new()
-harp$fetchRefs()
