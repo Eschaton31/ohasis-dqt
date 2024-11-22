196ed5bd5518860f75e6a917af087504b3bdb76c
John Benedict Palo <jrpalo@doh.gov.ph>
2024-11-22 16:04:27 +0800

    initial oct export

--- src/official/dsa/protects-upscale/05_export.R
+++ src/official/dsa/protects-upscale/05_export.R
@@ -1,6 +1,6 @@
-file_hts <- file.path("H:/Data Sharing/PROTECTS-UPSCALE", "hts_202401-202406.dta")
-file_art <- file.path("H:/Data Sharing/PROTECTS-UPSCALE", "tx_202401-202406.dta")
-file_prep <- file.path("H:/Data Sharing/PROTECTS-UPSCALE", "prep_202401-202406.dta")
+file_hts <- file.path("H:/Data Sharing/PROTECTS-UPSCALE", "hts_202401-202410_ahd-class.dta")
+file_art <- file.path("H:/Data Sharing/PROTECTS-UPSCALE", "tx_202401-202409.dta")
+file_prep <- file.path("H:/Data Sharing/PROTECTS-UPSCALE", "prep_202401-202409.dta")
 
 testing %>% format_stata %>% write_dta(file_hts)
 tx_gf %>% format_stata %>% write_dta(file_art)
