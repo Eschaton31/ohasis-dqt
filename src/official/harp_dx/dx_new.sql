fd8b0049578377ccdc31a9296a8c2dbd75a6e0c6
Gitea <gitea@fake.local>
2024-12-04 18:44:15 +0800

    fix to ignore null central ids (issue during generation of oct 2024 registry)

--- src/official/harp_dx/dx_new.sql
+++ src/official/harp_dx/dx_new.sql
@@ -26,4 +26,4 @@ FROM ohasis_lake.px_hiv_testing AS hts
          LEFT JOIN ohasis_warehouse.id_registry AS id ON pii.PATIENT_ID = id.PATIENT_ID
 WHERE (hts.CONFIRM_RESULT LIKE '1%'
     OR hts.CONFIRM_RESULT LIKE '5%')
-  AND COALESCE(id.CENTRAL_ID, pii.PATIENT_ID) NOT IN (SELECT CENTRAL_ID FROM ohasis_warehouse.harp_dx_old);
+  AND COALESCE(id.CENTRAL_ID, pii.PATIENT_ID) NOT IN (SELECT CENTRAL_ID FROM ohasis_warehouse.harp_dx_old WHERE CENTRAL_ID IS NOT NULL);
