46c26e5344433d34ab932f144288a1ee4b61c49d
John Benedict Palo <johnbenedictpalo@gmail.com>
2025-06-25 08:38:13 +0800

    import from rds

--- src/misc/ohasis2_migrations/facilities.R
+++ src/misc/ohasis2_migrations/facilities.R
@@ -102,6 +102,11 @@ facility_unit$download()
 
 match <- facility_unit$data$from %>%
    left_join(
+      y = data %>%
+         select(FACI_ID = faci_id, addr_reg, addr_prov, addr_munc),
+      by = join_by(FACI_ID)
+   ) %>%
+   left_join(
       y  = psgc_main %>%
          filter(!is.na(PSGC_OLD)) %>%
          select(REG = PSGC_OLD, ADDR_REG = PSGC),
@@ -120,6 +125,10 @@ match <- facility_unit$data$from %>%
       by = join_by(MUNC)
    ) %>%
    mutate(
+      ADDR_MUNC = coalesce(ADDR_MUNC, addr_munc),
+      ADDR_PROV = coalesce(ADDR_PROV, addr_prov),
+      ADDR_REG = coalesce(ADDR_REG, addr_reg),
+
       ADDR_PROV = if_else(!is.na(ADDR_MUNC), stri_pad_right(str_left(ADDR_MUNC, 5), 10, '0'), ADDR_PROV, ADDR_PROV),
       ADDR_REG  = if_else(!is.na(ADDR_PROV), stri_pad_right(str_left(ADDR_PROV, 2), 10, '0'), ADDR_REG, ADDR_REG),
 
@@ -127,6 +136,7 @@ match <- facility_unit$data$from %>%
       ADDR_PROV = if_else(str_left(REG, 2) == '99', '9999900000', ADDR_PROV, ADDR_PROV),
       ADDR_MUNC = if_else(str_left(REG, 2) == '99', '9999999000', ADDR_MUNC, ADDR_MUNC),
    ) %>%
+   select(-addr_reg, -addr_prov, -addr_munc) %>%
    mutate_at(vars(ADDR_REG, ADDR_PROV, ADDR_MUNC), ~coalesce(., "")) %>%
    rename(
       SUB_FACI_NAME_ALT = ALT_FACI_NAME,
@@ -150,6 +160,11 @@ facility_units <- QB$new(conn)$from("ohasis.facility_units")$get()
 users          <- QB$new(conn)$from("ohasis.users")$get()
 dbDisconnect(conn)
 
-write_rds(facilities,"H:/ohasis_dev-facilities.rds")
-write_rds(facility_units,"H:/ohasis_dev-facility_units.rds")
-write_rds(users,"H:/ohasis_dev-users.rds")
+write_rds(facilities, "H:/ohasis_dev-facilities.rds")
+write_rds(facility_units, "H:/ohasis_dev-facility_units.rds")
+write_rds(users, "H:/ohasis_dev-users.rds")
+
+data <- read_rds("H:/ohasis_dev-facilities.rds")
+conn <- connect('local')
+dbAppendTable(conn, "facilities", data)
+dbDisconnect(conn)
