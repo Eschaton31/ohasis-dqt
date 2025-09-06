ab6f2cac88d66340b10c46471674387c9876be9d
John Benedict Palo <johnbenedictpalo@gmail.com>
2025-09-06 16:55:09 +0800

    null typo

--- src/official/harp_dx/form_cfbs.sql
+++ src/official/harp_dx/form_cfbs.sql
@@ -5,4 +5,4 @@ from ohasis_warehouse.form_cfbs as hts_data
          left join ohasis_lake.id_registry on hts_data.patient_id = id_registry.patient_id
 where (px_hiv_testing.t0_result like '1%' or px_hiv_testing.t0_result is null or hts_data.rec_id in (select rec_id from ohasis_warehouse.dx_new))
   and coalesce(id_registry.central_id, hts_data.patient_id) in (select central_id from ohasis_warehouse.dx_new)
-  and hts_data.deleted_at is nul;
+  and hts_data.deleted_at is null;
