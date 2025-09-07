dfa4c0a2bce2fc1c4c5a2096f34ffed7f7a373c0
Gitea <gitea@fake.local>
2025-09-07 18:51:15 +0800

    remove bugged out interfaces

--- src/classes/DB.R
+++ src/classes/DB.R
@@ -187,19 +187,19 @@ DB <- R6Class(
          }
 
          # compare columns match, if not re-create table
-         names_data  <- sort(names(data))
-         names_table <- sort(dbListFields(db_conn, table_space))
-         if (!identical(names_data, names_table) & length(setdiff(names_data, names_table)) > 0) {
-            # get current records (assuming change is only added columns)
-            df <- dbReadTable(db_conn, table_space)
+         # names_data  <- sort(names(data))
+         # names_table <- sort(dbListFields(db_conn, table_space))
+         # if (!identical(names_data, names_table) & length(setdiff(names_data, names_table)) > 0) {
+            # # get current records (assuming change is only added columns)
+            # df <- dbReadTable(db_conn, table_space)
 
-            # recreate table using new columns
-            sql <- self$create(db_name, table_name, data, id_col)
-            dbExecute(db_conn, sql)
+            # # recreate table using new columns
+            # sql <- self$create(db_name, table_name, data, id_col)
+            # dbExecute(db_conn, sql)
 
-            # re-insert data
-            dbAppendTable(db_conn, table_sql, df)
-         }
+            # # re-insert data
+            # dbAppendTable(db_conn, table_sql, df)
+         # }
 
          data %<>%
             mutate_if(is.Date, ~format(., '%Y-%m-%d')) %>%
