22f4bcb84092e71bad52f44bf9f1a2416390380e
John Benedict Palo <johnbenedictpalo@gmail.com>
2025-09-19 15:50:42 +0800

    added renaming tolower of old outcome dataset

--- src/official/harp_tx/01_load_reqs.R
+++ src/official/harp_tx/01_load_reqs.R
@@ -279,7 +279,7 @@ update_dataset <- function(params, corr, forms, reprocess) {
    # clean if any for cleaning found
    if (nrow(corr$corr_outcome) > 0) {
       log_info("Performing cleaning on the outcome dataset.")
-      official$old_outcome <- apply_corrections(official$old_outcome, corr$corr_outcome %>%
+      official$old_outcome <- apply_corrections(official$old_outcome %>% rename_all(tolower), corr$corr_outcome %>%
          rename_all(tolower) %>%
          mutate(variable = tolower(variable)), names(art_id))
       # .cleaning_list(official$old_outcome, corr$corr_outcome %>% rename_all(tolower), "ART_ID", "integer")
