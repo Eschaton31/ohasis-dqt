abdc7a4ad282203e980278d92672bb0d9341273f
John Benedict Palo <johnbenedictpalo@gmail.com>
2026-07-27 08:44:53 +0800

    suppression warning moving as a whole

--- src/pipeline/stata.R
+++ src/pipeline/stata.R
@@ -2,7 +2,8 @@
 format_stata <- function(data) {
    data %<>%
       mutate_if(is.character, ~str_replace_all(., "\\\\", "\\")) %>%
-      mutate_if(bit64::is.integer64, ~suppress_warnings(as.integer(.), 'as.integer.integer64'))
+      mutate_if(bit64::is.integer64, ~as.integer(.)) %>%
+      suppress_warnings('as.integer.integer64')
 
    # convert to integers
    vars <- colnames(select_if(data, .predicate = is.numeric))
